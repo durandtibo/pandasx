adb5bac0862169a3bc267bfc849d60a32ca5856e
Thibaut Durand <durand.tibo@gmail.com>
2023-10-25 09:35:30 -0700

    Add `NullValueAnalyzer` (#14)

---
+++ src/pandasx/analyzer/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+__all__ = ["BaseAnalyzer", "NullValueAnalyzer"]
+
+from pandasx.analyzer.base import BaseAnalyzer
+from pandasx.analyzer.null import NullValueAnalyzer
