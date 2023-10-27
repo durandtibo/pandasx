da07c45665d87e27250c1a64daf416153f2581dc
Thibaut Durand <durand.tibo@gmail.com>
2023-10-27 07:17:44 -0700

    Add `ColumnDtypeSection`

--- src/flamme/section/__init__.py
+++ src/flamme/section/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-__all__ = ["BaseSection", "NullValueSection"]
+__all__ = ["BaseSection", "NullValueSection", "ColumnDtypeSection"]
 
 from flamme.section.base import BaseSection
+from flamme.section.dtype import ColumnDtypeSection
 from flamme.section.null import NullValueSection
