547b9e3a2ed9a5a88caa84aa9f35e64f6139b5d0
Thibaut Durand <durand.tibo@gmail.com>
2023-10-27 07:49:42 -0700

    Add `ColumnTypeSection`

--- src/flamme/section/__init__.py
+++ src/flamme/section/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-__all__ = ["BaseSection", "NullValueSection", "ColumnDtypeSection"]
+__all__ = ["BaseSection", "NullValueSection", "ColumnDtypeSection", "ColumnTypeSection"]
 
 from flamme.section.base import BaseSection
-from flamme.section.dtype import ColumnDtypeSection
+from flamme.section.dtype import ColumnDtypeSection, ColumnTypeSection
 from flamme.section.null import NullValueSection
--- tests/unit/section/test_dtype.py
+++ tests/unit/section/test_dtype.py
@@ -4,14 +4,14 @@ from coola import objects_are_allclose
 from jinja2 import Template
 from pandas import Series
 
-from flamme.section import ColumnDtypeSection
+from flamme.section import ColumnDtypeSection, ColumnTypeSection
 
 ########################################
 #     Tests for ColumnDtypeSection     #
 ########################################
 
 
-def test_null_value_section_get_statistics() -> None:
+def test_column_dtype_section_get_statistics() -> None:
     output = ColumnDtypeSection(
         dtypes=Series({"col1": float, "col2": int, "col3": str}),
     )
@@ -21,26 +21,19 @@ def test_null_value_section_get_statistics() -> None:
     )
 
 
-def test_null_value_section_get_statistics_empty_row() -> None:
-    output = ColumnDtypeSection(
-        dtypes=Series({}),
-    )
-    assert objects_are_allclose(output.get_statistics(), {})
-
-
-def test_null_value_section_get_statistics_empty_column() -> None:
+def test_column_dtype_section_get_statistics_empty() -> None:
     output = ColumnDtypeSection(dtypes=Series({}))
     assert objects_are_allclose(output.get_statistics(), {})
 
 
-def test_null_value_section_render_html_body() -> None:
+def test_column_dtype_section_render_html_body() -> None:
     output = ColumnDtypeSection(
         dtypes=Series({"col1": float, "col2": int, "col3": str}),
     )
     assert isinstance(Template(output.render_html_body()).render(), str)
 
 
-def test_null_value_section_render_html_body_args() -> None:
+def test_column_dtype_section_render_html_body_args() -> None:
     output = ColumnDtypeSection(
         dtypes=Series({"col1": float, "col2": int, "col3": str}),
     )
@@ -49,23 +42,80 @@ def test_null_value_section_render_html_body_args() -> None:
     )
 
 
-def test_null_value_section_render_html_body_empty() -> None:
+def test_column_dtype_section_render_html_body_empty() -> None:
     output = ColumnDtypeSection(
         dtypes=Series({}),
     )
     assert isinstance(Template(output.render_html_body()).render(), str)
 
 
-def test_null_value_section_render_html_toc() -> None:
+def test_column_dtype_section_render_html_toc() -> None:
     output = ColumnDtypeSection(
         dtypes=Series({"col1": float, "col2": int, "col3": str}),
     )
     assert isinstance(Template(output.render_html_toc()).render(), str)
 
 
-def test_null_value_section_render_html_toc_args() -> None:
+def test_column_dtype_section_render_html_toc_args() -> None:
     output = ColumnDtypeSection(
         dtypes=Series({"col1": float, "col2": int, "col3": str}),
+    )
+    assert isinstance(
+        Template(output.render_html_toc(number="1.", tags=["meow"], depth=1)).render(), str
+    )
+
+
+#######################################
+#     Tests for ColumnTypeSection     #
+#######################################
+
+
+def test_column_type_section_get_statistics() -> None:
+    output = ColumnTypeSection(
+        types={"col1": {float}, "col2": {int}, "col3": {str, type(None)}},
+    )
+    assert objects_are_allclose(
+        output.get_statistics(),
+        {"col1": {float}, "col2": {int}, "col3": {str, type(None)}},
+    )
+
+
+def test_column_type_section_get_statistics_empty() -> None:
+    output = ColumnTypeSection(types={})
+    assert objects_are_allclose(output.get_statistics(), {})
+
+
+def test_column_type_section_render_html_body() -> None:
+    output = ColumnTypeSection(
+        types={"col1": {float}, "col2": {int}, "col3": {str}},
+    )
+    assert isinstance(Template(output.render_html_body()).render(), str)
+
+
+def test_column_type_section_render_html_body_args() -> None:
+    output = ColumnTypeSection(
+        types={"col1": {float}, "col2": {int}, "col3": {str}},
+    )
+    assert isinstance(
+        Template(output.render_html_body(number="1.", tags=["meow"], depth=1)).render(), str
+    )
+
+
+def test_column_type_section_render_html_body_empty() -> None:
+    output = ColumnTypeSection(
+        types={},
+    )
+    assert isinstance(Template(output.render_html_body()).render(), str)
+
+
+def test_column_type_section_render_html_toc() -> None:
+    output = ColumnTypeSection(types={"col1": {float}, "col2": {int}, "col3": {str}})
+    assert isinstance(Template(output.render_html_toc()).render(), str)
+
+
+def test_column_type_section_render_html_toc_args() -> None:
+    output = ColumnTypeSection(
+        types={"col1": {float}, "col2": {int}, "col3": {str}},
     )
     assert isinstance(
         Template(output.render_html_toc(number="1.", tags=["meow"], depth=1)).render(), str
