bda5ce0895181827bbbf03cd1e2bd0d3ede7114b
Thibaut Durand <durand.tibo@gmail.com>
2023-10-28 08:52:32 -0700

    Add `MappingAnalyzer` (#28)

--- src/flamme/analyzer/__init__.py
+++ src/flamme/analyzer/__init__.py
@@ -4,11 +4,13 @@ __all__ = [
     "BaseAnalyzer",
     "ColumnDtypeAnalyzer",
     "ColumnTypeAnalyzer",
+    "MappingAnalyzer",
     "NanValueAnalyzer",
     "NullValueAnalyzer",
 ]
 
 from flamme.analyzer.base import BaseAnalyzer
 from flamme.analyzer.dtype import ColumnDtypeAnalyzer, ColumnTypeAnalyzer
+from flamme.analyzer.mapping import MappingAnalyzer
 from flamme.analyzer.nan import NanValueAnalyzer
 from flamme.analyzer.null import NullValueAnalyzer
