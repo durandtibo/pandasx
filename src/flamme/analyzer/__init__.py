c0e79e74a51edfc7155eda424508dcfc15c1ba0f
Thibaut Durand <durand.tibo@gmail.com>
2023-10-28 07:36:47 -0700

    Add `NanValueAnalyzer` (#26)

--- src/flamme/analyzer/__init__.py
+++ src/flamme/analyzer/__init__.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
 
-__all__ = ["BaseAnalyzer", "NullValueAnalyzer", "ColumnDtypeAnalyzer", "ColumnTypeAnalyzer"]
+__all__ = [
+    "BaseAnalyzer",
+    "ColumnDtypeAnalyzer",
+    "ColumnTypeAnalyzer",
+    "NanValueAnalyzer",
+    "NullValueAnalyzer",
+]
 
 from flamme.analyzer.base import BaseAnalyzer
 from flamme.analyzer.dtype import ColumnDtypeAnalyzer, ColumnTypeAnalyzer
+from flamme.analyzer.nan import NanValueAnalyzer
 from flamme.analyzer.null import NullValueAnalyzer
