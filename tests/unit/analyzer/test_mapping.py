6def710a07cd24a848e270ffc0cc43210b034001
Thibaut Durand <durand.tibo@gmail.com>
2023-10-28 08:07:46 -0700

    Add `MappingAnalyzer`

---
+++ tests/unit/analyzer/test_mapping.py
@@ -0,0 +1,80 @@
+import numpy as np
+from coola import objects_are_allclose
+from pandas import DataFrame
+
+from flamme.analyzer import MappingAnalyzer, NanValueAnalyzer
+from flamme.section import SectionDict
+
+#####################################
+#     Tests for MappingAnalyzer     #
+#####################################
+
+
+def test_mapping_analyzer_str() -> None:
+    assert str(MappingAnalyzer({})).startswith("MappingAnalyzer(")
+
+
+def test_mapping_analyzer_get_statistics() -> None:
+    section = MappingAnalyzer(
+        {
+            "section1": NanValueAnalyzer(),
+            "section2": NanValueAnalyzer(),
+        }
+    ).analyze(
+        DataFrame(
+            {
+                "float": np.array([1.2, 4.2, np.nan, 2.2]),
+                "int": np.array([np.nan, 1, 0, 1]),
+                "str": np.array(["A", "B", None, np.nan]),
+            }
+        )
+    )
+    assert isinstance(section, SectionDict)
+    assert objects_are_allclose(
+        section.get_statistics(),
+        {
+            "section1": {
+                "columns": ("float", "int", "str"),
+                "nan_count": (1, 1, 2),
+                "total_count": (4, 4, 4),
+            },
+            "section2": {
+                "columns": ("float", "int", "str"),
+                "nan_count": (1, 1, 2),
+                "total_count": (4, 4, 4),
+            },
+        },
+    )
+
+
+def test_mapping_analyzer_get_statistics_empty() -> None:
+    section = MappingAnalyzer(
+        {
+            "section1": NanValueAnalyzer(),
+            "section2": NanValueAnalyzer(),
+        }
+    ).analyze(
+        DataFrame(
+            {
+                "float": np.array([]),
+                "int": np.array([]),
+                "str": np.array([]),
+            }
+        )
+    )
+    assert isinstance(section, SectionDict)
+    assert objects_are_allclose(
+        section.get_statistics(),
+        {
+            "section1": {
+                "columns": ("float", "int", "str"),
+                "nan_count": (0, 0, 0),
+                "total_count": (0, 0, 0),
+            },
+            "section2": {
+                "columns": ("float", "int", "str"),
+                "nan_count": (0, 0, 0),
+                "total_count": (0, 0, 0),
+            },
+        },
+    )
