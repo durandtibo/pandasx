a52b1aad770a8b594ff7ea1e8afb0a9c49bbbc56
Thibaut Durand <durand.tibo@gmail.com>
2023-10-28 09:55:39 -0700

    Improve column type analyzer

--- tests/unit/section/test_dtype.py
+++ tests/unit/section/test_dtype.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 from coola import objects_are_allclose
 from jinja2 import Template
+from numpy import dtype
 from pandas import Series
+from pytest import raises
 
 from flamme.section import ColumnDtypeSection, ColumnTypeSection
 
@@ -70,31 +72,50 @@ def test_column_dtype_section_render_html_toc_args() -> None:
 #######################################
 
 
+def test_column_type_section_incorrect_different_key() -> None:
+    with raises(RuntimeError, match="The keys of dtypes and types do not match:"):
+        ColumnTypeSection(
+            dtypes={"col": dtype("float64"), "int": dtype("float64"), "str": dtype("O")},
+            types={"float": {float}, "int": {int}, "str": {str, type(None)}},
+        )
+
+
+def test_column_type_section_incorrect_missing_key() -> None:
+    with raises(RuntimeError, match="The keys of dtypes and types do not match:"):
+        ColumnTypeSection(
+            dtypes={"int": dtype("float64"), "str": dtype("O")},
+            types={"float": {float}, "int": {int}, "str": {str, type(None)}},
+        )
+
+
 def test_column_type_section_get_statistics() -> None:
     output = ColumnTypeSection(
-        types={"col1": {float}, "col2": {int}, "col3": {str, type(None)}},
+        dtypes={"float": dtype("float64"), "int": dtype("float64"), "str": dtype("O")},
+        types={"float": {float}, "int": {int}, "str": {str, type(None)}},
     )
     assert objects_are_allclose(
         output.get_statistics(),
-        {"col1": {float}, "col2": {int}, "col3": {str, type(None)}},
+        {"float": {float}, "int": {int}, "str": {str, type(None)}},
     )
 
 
 def test_column_type_section_get_statistics_empty() -> None:
-    output = ColumnTypeSection(types={})
+    output = ColumnTypeSection(dtypes={}, types={})
     assert objects_are_allclose(output.get_statistics(), {})
 
 
 def test_column_type_section_render_html_body() -> None:
     output = ColumnTypeSection(
-        types={"col1": {float}, "col2": {int}, "col3": {str}},
+        dtypes={"float": dtype("float64"), "int": dtype("float64"), "str": dtype("O")},
+        types={"float": {float}, "int": {int}, "str": {str}},
     )
     assert isinstance(Template(output.render_html_body()).render(), str)
 
 
 def test_column_type_section_render_html_body_args() -> None:
     output = ColumnTypeSection(
-        types={"col1": {float}, "col2": {int}, "col3": {str}},
+        dtypes={"float": dtype("float64"), "int": dtype("float64"), "str": dtype("O")},
+        types={"float": {float}, "int": {int}, "str": {str}},
     )
     assert isinstance(
         Template(output.render_html_body(number="1.", tags=["meow"], depth=1)).render(), str
@@ -102,20 +123,22 @@ def test_column_type_section_render_html_body_args() -> None:
 
 
 def test_column_type_section_render_html_body_empty() -> None:
-    output = ColumnTypeSection(
-        types={},
-    )
+    output = ColumnTypeSection(dtypes={}, types={})
     assert isinstance(Template(output.render_html_body()).render(), str)
 
 
 def test_column_type_section_render_html_toc() -> None:
-    output = ColumnTypeSection(types={"col1": {float}, "col2": {int}, "col3": {str}})
+    output = ColumnTypeSection(
+        dtypes={"float": dtype("float64"), "int": dtype("float64"), "str": dtype("O")},
+        types={"float": {float}, "int": {int}, "str": {str}},
+    )
     assert isinstance(Template(output.render_html_toc()).render(), str)
 
 
 def test_column_type_section_render_html_toc_args() -> None:
     output = ColumnTypeSection(
-        types={"col1": {float}, "col2": {int}, "col3": {str}},
+        dtypes={"float": dtype("float64"), "int": dtype("float64"), "str": dtype("O")},
+        types={"float": {float}, "int": {int}, "str": {str}},
     )
     assert isinstance(
         Template(output.render_html_toc(number="1.", tags=["meow"], depth=1)).render(), str
