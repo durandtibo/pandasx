97ca988bc99eff58868d1bb162ad697c4849a920
Thibaut Durand <durand.tibo@gmail.com>
2023-10-28 07:14:25 -0700

    Add `NanValueSection` (#25)

--- src/flamme/section/__init__.py
+++ src/flamme/section/__init__.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
 
-__all__ = ["BaseSection", "NullValueSection", "ColumnDtypeSection", "ColumnTypeSection"]
+__all__ = [
+    "BaseSection",
+    "ColumnDtypeSection",
+    "ColumnTypeSection",
+    "NanValueSection",
+    "NullValueSection",
+]
 
 from flamme.section.base import BaseSection
 from flamme.section.dtype import ColumnDtypeSection, ColumnTypeSection
+from flamme.section.nan import NanValueSection
 from flamme.section.null import NullValueSection
