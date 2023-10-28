efdafcbe59dab77e6fd99c4e80493ab1920d4e91
Thibaut Durand <durand.tibo@gmail.com>
2023-10-28 10:14:37 -0700

    Remove column data type analyzer/section

--- src/flamme/analyzer/dtype.py
+++ src/flamme/analyzer/dtype.py
@@ -1,42 +1,12 @@
 from __future__ import annotations
 
-__all__ = ["ColumnDtypeAnalyzer"]
+__all__ = ["ColumnTypeAnalyzer"]
 
 from pandas import DataFrame
 
 from flamme.analyzer.base import BaseAnalyzer
-from flamme.section import ColumnDtypeSection, ColumnTypeSection
+from flamme.section import ColumnTypeSection
 from flamme.utils.dtype import column_types
-
-
-class ColumnDtypeAnalyzer(BaseAnalyzer):
-    r"""Implements a column data type analyzer.
-
-    Example usage:
-
-    .. code-block:: pycon
-
-        >>> import numpy as np
-        >>> import pandas as pd
-        >>> from flamme.analyzer import ColumnDtypeAnalyzer
-        >>> analyzer = ColumnDtypeAnalyzer()
-        >>> analyzer
-        ColumnDtypeAnalyzer()
-        >>> df = pd.DataFrame(
-        ...     {
-        ...         "int": np.array([np.nan, 1, 0, 1]),
-        ...         "float": np.array([1.2, 4.2, np.nan, 2.2]),
-        ...         "str": np.array(["A", "B", None, np.nan]),
-        ...     }
-        ... )
-        >>> section = analyzer.analyze(df)
-    """
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}()"
-
-    def analyze(self, df: DataFrame) -> ColumnDtypeSection:
-        return ColumnDtypeSection(df.dtypes)
 
 
 class ColumnTypeAnalyzer(BaseAnalyzer):
--- src/flamme/section/__init__.py
+++ src/flamme/section/__init__.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 __all__ = [
     "BaseSection",
-    "ColumnDtypeSection",
     "ColumnTypeSection",
     "NanValueSection",
     "NullValueSection",
@@ -10,7 +9,7 @@ __all__ = [
 ]
 
 from flamme.section.base import BaseSection
-from flamme.section.dtype import ColumnDtypeSection, ColumnTypeSection
+from flamme.section.dtype import ColumnTypeSection
 from flamme.section.mapping import SectionDict
 from flamme.section.nan import NanValueSection
 from flamme.section.null import NullValueSection
