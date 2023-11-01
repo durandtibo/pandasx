3b331ad66966e89a02e181c94c275944314ded29
Thibaut Durand <durand.tibo@gmail.com>
2023-10-31 17:36:35 -0700

    Remove `NanValueAnalyzer`

--- src/flamme/analyzer/__init__.py
+++ src/flamme/analyzer/__init__.py
@@ -6,7 +6,6 @@ __all__ = [
     "DiscreteDistributionAnalyzer",
     "FilteredAnalyzer",
     "MappingAnalyzer",
-    "NanValueAnalyzer",
     "NullValueAnalyzer",
     "is_analyzer_config",
     "setup_analyzer",
@@ -17,5 +16,4 @@ from flamme.analyzer.discrete import DiscreteDistributionAnalyzer
 from flamme.analyzer.dtype import ColumnTypeAnalyzer
 from flamme.analyzer.filter import FilteredAnalyzer
 from flamme.analyzer.mapping import MappingAnalyzer
-from flamme.analyzer.nan import NanValueAnalyzer
 from flamme.analyzer.null import NullValueAnalyzer
