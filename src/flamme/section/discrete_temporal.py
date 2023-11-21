a74069b0e32e5f705916c313fb25879cafe3b034
Thibaut Durand <durand.tibo@gmail.com>
2023-11-21 15:13:40 -0800

    Add partial `TemporalDiscreteDistributionSection`

---
+++ src/flamme/section/discrete_temporal.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+
+__all__ = ["TemporalDiscreteDistributionSection"]
+
+from collections.abc import Sequence
+
+from jinja2 import Template
+from pandas import DataFrame
+
+from flamme.section.base import BaseSection
+from flamme.section.utils import (
+    GO_TO_TOP,
+    render_html_toc,
+    tags2id,
+    tags2title,
+    valid_h_tag,
+)
+
+
+class TemporalDiscreteDistributionSection(BaseSection):
+    r"""Implements a section that analyzes the temporal distribution of a
+    column with discrete values.
+
+    Args:
+    ----
+        df (``pandas.DataFrame``): Specifies the DataFrame to analyze.
+        column (str): Specifies the column of the DataFrame to analyze.
+        dt_column (str): Specifies the datetime column used to analyze
+            the temporal distribution.
+        period (str): Specifies the temporal period e.g. monthly or
+            daily.
+        log_y (bool, optional): If ``True``, it represents the bars
+            with a log scale. Default: ``False``
+    """
+
+    def __init__(
+        self,
+        df: DataFrame,
+        column: str,
+        dt_column: str,
+        period: str,
+        log_y: bool = False,
+    ) -> None:
+        self._df = df
+        self._column = column
+        self._dt_column = dt_column
+        self._period = period
+        self._log_y = log_y
+
+    @property
+    def column(self) -> str:
+        return self._column
+
+    @property
+    def dt_column(self) -> str:
+        return self._dt_column
+
+    @property
+    def log_y(self) -> bool:
+        return self._log_y
+
+    @property
+    def period(self) -> str:
+        return self._period
+
+    def get_statistics(self) -> dict:
+        return {}
+
+    def render_html_body(self, number: str = "", tags: Sequence[str] = (), depth: int = 0) -> str:
+        return Template(self._create_template()).render(
+            {
+                "go_to_top": GO_TO_TOP,
+                "id": tags2id(tags),
+                "depth": valid_h_tag(depth + 1),
+                "title": tags2title(tags),
+                "section": number,
+                "column": self._column,
+                "dt_column": self._dt_column,
+                "period": self._period,
+            }
+        )
+
+    def render_html_toc(
+        self, number: str = "", tags: Sequence[str] = (), depth: int = 0, max_depth: int = 1
+    ) -> str:
+        return render_html_toc(number=number, tags=tags, depth=depth, max_depth=max_depth)
+
+    def _create_template(self) -> str:
+        return """
+<h{{depth}} id="{{id}}">{{section}} {{title}} </h{{depth}}>
+
+{{go_to_top}}
+
+<p style="margin-top: 1rem;">
+This section analyzes the temporal distribution of column {{column}} by using the column {{dt_column}}.
+
+{{figure}}
+
+{{table}}
+<p style="margin-top: 1rem;">
+"""
