e51165c783ce2228ff383a982dd36b7f7c4f6829
Thibaut Durand <durand.tibo@gmail.com>
2023-11-22 17:55:18 +1300

    Add `setup_ingestor` (#93)

--- src/flamme/ingestor/__init__.py
+++ src/flamme/ingestor/__init__.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
 
-__all__ = ["BaseIngestor", "CsvIngestor", "ParquetIngestor"]
+__all__ = [
+    "BaseIngestor",
+    "CsvIngestor",
+    "ParquetIngestor",
+    "is_ingestor_config",
+    "setup_ingestor",
+]
 
-from flamme.ingestor.base import BaseIngestor
+from flamme.ingestor.base import BaseIngestor, is_ingestor_config, setup_ingestor
 from flamme.ingestor.csv import CsvIngestor
 from flamme.ingestor.parquet import ParquetIngestor
