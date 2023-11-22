82423af265681d0703763fac78f24e9ac1e10522
Thibaut Durand <durand.tibo@gmail.com>
2023-11-23 10:27:20 +1300

    Add `BaseReporter` (#99)

---
+++ src/flamme/reporter/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+__all__ = ["BaseReporter", "is_reporter_config", "setup_reporter"]
+
+from flamme.reporter.base import BaseReporter, is_reporter_config, setup_reporter
