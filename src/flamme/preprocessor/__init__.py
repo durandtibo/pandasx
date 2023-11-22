d06c3596184f1a8fd4f49c0086f7c94d68819090
Thibaut Durand <durand.tibo@gmail.com>
2023-11-21 18:24:27 -0800

    Add `ToNumericPreprocessor`

--- src/flamme/preprocessor/__init__.py
+++ src/flamme/preprocessor/__init__.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
-__all__ = ["BasePreprocessor", "is_preprocessor_config", "setup_preprocessor"]
+__all__ = [
+    "BasePreprocessor",
+    "ToNumericPreprocessor",
+    "is_preprocessor_config",
+    "setup_preprocessor",
+]
 
 from flamme.preprocessor.base import (
     BasePreprocessor,
     is_preprocessor_config,
     setup_preprocessor,
 )
+from flamme.preprocessor.numeric import ToNumericPreprocessor
