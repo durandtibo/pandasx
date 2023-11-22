8dc74136de3cd1ed74a9cd56627065242e95f2c9
Thibaut Durand <durand.tibo@gmail.com>
2023-11-22 14:00:59 -0800

    Add missing tests to reporters

---
+++ tests/unit/reporter/test_base.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+
+import logging
+from collections import Counter
+from pathlib import Path
+
+from objectory import OBJECT_TARGET
+from pytest import LogCaptureFixture
+
+from flamme.analyzer import NullValueAnalyzer
+from flamme.ingestor import ParquetIngestor
+from flamme.preprocessor import SequentialPreprocessor
+from flamme.reporter import Reporter, is_reporter_config, setup_reporter
+
+########################################
+#     Tests for is_reporter_config     #
+########################################
+
+
+def test_is_reporter_config_true() -> None:
+    assert is_reporter_config(
+        {
+            OBJECT_TARGET: "flamme.reporter.Reporter",
+            "ingestor": {OBJECT_TARGET: "flamme.ingestor.CsvIngestor", "path": "/path/to/data.csv"},
+            "preprocessor": {
+                OBJECT_TARGET: "flamme.preprocessor.ToNumericPreprocessor",
+                "columns": ["col1", "col3"],
+            },
+            "analyzer": {OBJECT_TARGET: "flamme.analyzer.NullValueAnalyzer"},
+            "report_path": "/path/to/report.html",
+        }
+    )
+
+
+def test_is_reporter_config_false() -> None:
+    assert not is_reporter_config({OBJECT_TARGET: "collections.Counter"})
+
+
+####################################
+#     Tests for setup_reporter     #
+####################################
+
+
+def test_setup_reporter_object(tmp_path: Path) -> None:
+    reporter = Reporter(
+        ingestor=ParquetIngestor(tmp_path.joinpath("data.parquet")),
+        preprocessor=SequentialPreprocessor(preprocessors=[]),
+        analyzer=NullValueAnalyzer(),
+        report_path=tmp_path.joinpath("report.html"),
+    )
+    assert setup_reporter(reporter) is reporter
+
+
+def test_setup_reporter_dict() -> None:
+    assert isinstance(
+        setup_reporter(
+            {
+                OBJECT_TARGET: "flamme.reporter.Reporter",
+                "ingestor": {
+                    OBJECT_TARGET: "flamme.ingestor.CsvIngestor",
+                    "path": "/path/to/data.csv",
+                },
+                "preprocessor": {
+                    OBJECT_TARGET: "flamme.preprocessor.ToNumericPreprocessor",
+                    "columns": ["col1", "col3"],
+                },
+                "analyzer": {OBJECT_TARGET: "flamme.analyzer.NullValueAnalyzer"},
+                "report_path": "/path/to/report.html",
+            }
+        ),
+        Reporter,
+    )
+
+
+def test_setup_reporter_incorrect_type(caplog: LogCaptureFixture) -> None:
+    with caplog.at_level(level=logging.WARNING):
+        assert isinstance(setup_reporter({OBJECT_TARGET: "collections.Counter"}), Counter)
+        assert caplog.messages
