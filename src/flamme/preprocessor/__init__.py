bf9b88ff174c8bd515687b953d2ec485c80396be
Thibaut Durand <durand.tibo@gmail.com>
2023-11-22 14:48:44 +1300

    Add `BasePreprocessor` (#87)

---
+++ src/flamme/preprocessor/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+__all__ = ["BasePreprocessor", "is_preprocessor_config", "setup_preprocessor"]
+
+from flamme.preprocessor.base import (
+    BasePreprocessor,
+    is_preprocessor_config,
+    setup_preprocessor,
+)
