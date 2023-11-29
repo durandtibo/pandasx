1840b76109980f18261fb9794ad3ff46865594a7
Thibaut Durand <durand.tibo@gmail.com>
2023-11-28 23:02:46 -0800

    Add `ColumnSubsetAnalyzer` (#111)

--- src/flamme/analyzer/__init__.py
+++ src/flamme/analyzer/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 __all__ = [
     "BaseAnalyzer",
+    "ColumnSubsetAnalyzer",
     "ColumnTypeAnalyzer",
     "ContinuousDistributionAnalyzer",
     "DiscreteDistributionAnalyzer",
@@ -17,6 +18,7 @@ __all__ = [
 ]
 
 from flamme.analyzer.base import BaseAnalyzer, is_analyzer_config, setup_analyzer
+from flamme.analyzer.column import ColumnSubsetAnalyzer
 from flamme.analyzer.continuous import (
     ContinuousDistributionAnalyzer,
     TemporalContinuousDistributionAnalyzer,
