868efd9996c460cb72fac42c5f3c33ab28a0ee50
Thibaut Durand <durand.tibo@gmail.com>
2023-12-07 11:28:22 -0800

    Add `ToDatetimeSeriesTransformer` (#154)

--- src/flamme/transformer/series/__init__.py
+++ src/flamme/transformer/series/__init__.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 __all__ = [
     "BaseSeriesTransformer",
-    "is_series_transformer_config",
-    "setup_series_transformer",
+    "ToDatetime",
+    "ToDatetimeSeriesTransformer",
     "ToNumeric",
     "ToNumericSeriesTransformer",
+    "is_series_transformer_config",
+    "setup_series_transformer",
 ]
 
 from flamme.transformer.series.base import (
@@ -13,5 +15,7 @@ from flamme.transformer.series.base import (
     is_series_transformer_config,
     setup_series_transformer,
 )
+from flamme.transformer.series.datetime import ToDatetimeSeriesTransformer
+from flamme.transformer.series.datetime import ToDatetimeSeriesTransformer as ToDatetime
 from flamme.transformer.series.numeric import ToNumericSeriesTransformer
 from flamme.transformer.series.numeric import ToNumericSeriesTransformer as ToNumeric
