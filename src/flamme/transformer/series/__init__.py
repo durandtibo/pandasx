bd45397a5f45534bd25cc5da53482282fd7299a3
Thibaut Durand <durand.tibo@gmail.com>
2023-12-06 16:38:55 -0800

    Add `BaseSeriesTransformer`

---
+++ src/flamme/transformer/series/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+__all__ = ["BaseSeriesTransformer", "is_series_transformer_config", "setup_series_transformer"]
+
+from flamme.transformer.series.base import (
+    BaseSeriesTransformer,
+    is_series_transformer_config,
+    setup_series_transformer,
+)
