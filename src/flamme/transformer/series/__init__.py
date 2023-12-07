02215752ab1fcc9e9b665ca47fd037f837866532
Thibaut Durand <durand.tibo@gmail.com>
2023-12-06 17:00:54 -0800

    Add `ToNumericSeriesTransformer` (#152)

--- src/flamme/transformer/series/__init__.py
+++ src/flamme/transformer/series/__init__.py
@@ -1,9 +1,17 @@
 from __future__ import annotations
 
-__all__ = ["BaseSeriesTransformer", "is_series_transformer_config", "setup_series_transformer"]
+__all__ = [
+    "BaseSeriesTransformer",
+    "is_series_transformer_config",
+    "setup_series_transformer",
+    "ToNumeric",
+    "ToNumericSeriesTransformer",
+]
 
 from flamme.transformer.series.base import (
     BaseSeriesTransformer,
     is_series_transformer_config,
     setup_series_transformer,
 )
+from flamme.transformer.series.numeric import ToNumericSeriesTransformer
+from flamme.transformer.series.numeric import ToNumericSeriesTransformer as ToNumeric
