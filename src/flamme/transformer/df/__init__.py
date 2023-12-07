bd1b36fbdf7adb9c886fdb7c9cd35ccc1736b6a8
Thibaut Durand <durand.tibo@gmail.com>
2023-12-07 11:47:15 -0800

    Add `BaseDataFrameTransformer`

---
+++ src/flamme/transformer/df/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+__all__ = [
+    "BaseDataFrameTransformer",
+    "is_dataframe_transformer_config",
+    "setup_dataframe_transformer",
+]
+
+from flamme.transformer.df.base import (
+    BaseDataFrameTransformer,
+    is_dataframe_transformer_config,
+    setup_dataframe_transformer,
+)
