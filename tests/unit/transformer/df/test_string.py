dc5547ed95d7c1e6f1915961d9fa68665ecf5f57
Thibaut Durand <durand.tibo@gmail.com>
2023-12-09 14:17:18 -0800

    Testing and deps improvements (#175)

--- tests/unit/transformer/df/test_string.py
+++ tests/unit/transformer/df/test_string.py
@@ -39,3 +39,34 @@ def test_strip_str_dataframe_transformer_transform() -> None:
             }
         ),
     )
+
+
+def test_strip_str_dataframe_transformer_transform_none() -> None:
+    df = pd.DataFrame(
+        {
+            "col1": [1, 2, 3, 4, "  ", None, 42, 4.2],
+            "col2": ["1", "2", "3", "4", "5", None, 42, 4.2],
+            "col3": ["a ", " b", "  c  ", "d", "e", None, 42, 4.2],
+            "col4": ["a ", " b", "  c  ", "d", "e", None, 42, 4.2],
+        }
+    )
+    transformer = StripString(columns=["col1", "col3"])
+    df = transformer.transform(df)
+    assert_frame_equal(
+        df,
+        pd.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, "", None, 42, 4.2],
+                "col2": ["1", "2", "3", "4", "5", None, 42, 4.2],
+                "col3": ["a", "b", "c", "d", "e", None, 42, 4.2],
+                "col4": ["a ", " b", "  c  ", "d", "e", None, 42, 4.2],
+            }
+        ),
+    )
+
+
+def test_strip_str_dataframe_transformer_transform_empty() -> None:
+    df = pd.DataFrame({})
+    transformer = StripString(columns=[])
+    df = transformer.transform(df)
+    assert_frame_equal(df, pd.DataFrame({}))
