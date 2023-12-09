4651f32433c31a828f4a5aeefda9cc5724adc3ad
Thibaut Durand <durand.tibo@gmail.com>
2023-12-09 14:10:41 -0800

    Testing and deps improvements

--- tests/unit/transformer/series/test_string.py
+++ tests/unit/transformer/series/test_string.py
@@ -18,3 +18,15 @@ def test_strip_string_series_transformer_transform() -> None:
     transformer = StripString()
     series = transformer.transform(pd.Series(["a ", " b", "  c  ", " d ", "e"]))
     assert_series_equal(series, pd.Series(["a", "b", "c", "d", "e"]))
+
+
+def test_strip_string_series_transformer_transform_none() -> None:
+    transformer = StripString()
+    series = transformer.transform(pd.Series(["a ", " b", "  c  ", " d ", "e", None, 42, 4.2]))
+    assert_series_equal(series, pd.Series(["a", "b", "c", "d", "e", None, 42, 4.2]))
+
+
+def test_strip_string_series_transformer_transform_empty() -> None:
+    transformer = StripString()
+    series = transformer.transform(pd.Series([]))
+    assert_series_equal(series, pd.Series([]))
