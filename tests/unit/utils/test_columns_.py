7e8a2324594e73c5b0d67109154f091b306bd838
Thibaut Durand <durand.tibo@gmail.com>
2023-12-10 10:17:23 -0800

    Improve columns (#177)

--- tests/unit/utils/test_columns_.py
+++ tests/unit/utils/test_columns_.py
@@ -3,8 +3,13 @@ from __future__ import annotations
 from objectory import OBJECT_TARGET
 from pytest import mark
 
+from flamme.analyzer import (
+    BaseAnalyzer,
+    ColumnContinuousAnalyzer,
+    ColumnDiscreteAnalyzer,
+)
 from flamme.transformer.series import BaseSeriesTransformer, StripString, ToNumeric
-from flamme.utils.columns_ import Column, NumericColumn, StringColumn
+from flamme.utils.columns_ import Column
 
 ############################
 #     Tests for Column     #
@@ -12,12 +17,40 @@ from flamme.utils.columns_ import Column, NumericColumn, StringColumn
 
 
 def test_column_str() -> None:
-    assert str(Column(can_be_null=False, transformer=ToNumeric())).startswith("Column(")
+    assert str(
+        Column(
+            can_be_null=False,
+            analyzer=ColumnContinuousAnalyzer(column="col"),
+            transformer=ToNumeric(),
+        )
+    ).startswith("Column(")
 
 
 @mark.parametrize("can_be_null", (True, False))
 def test_column_can_be_null(can_be_null: bool) -> None:
-    assert Column(can_be_null=can_be_null, transformer=ToNumeric()).can_be_null == can_be_null
+    assert (
+        Column(
+            can_be_null=can_be_null,
+            analyzer=ColumnContinuousAnalyzer(column="col"),
+            transformer=ToNumeric(),
+        ).can_be_null
+        == can_be_null
+    )
+
+
+@mark.parametrize(
+    "analyzer",
+    (
+        ColumnContinuousAnalyzer(column="col"),
+        ColumnDiscreteAnalyzer(column="col"),
+        {OBJECT_TARGET: "flamme.analyzer.ColumnContinuousAnalyzer", "column": "col"},
+    ),
+)
+def test_column_get_analyzer(analyzer: BaseAnalyzer | dict) -> None:
+    assert isinstance(
+        Column(can_be_null=False, analyzer=analyzer, transformer=ToNumeric()).get_analyzer(),
+        BaseAnalyzer,
+    )
 
 
 @mark.parametrize(
@@ -26,45 +59,10 @@ def test_column_can_be_null(can_be_null: bool) -> None:
 )
 def test_column_get_transformer(transformer: BaseSeriesTransformer | dict) -> None:
     assert isinstance(
-        Column(can_be_null=False, transformer=transformer).get_transformer(), BaseSeriesTransformer
-    )
-
-
-##################################
-#     Tests for NumericColumn     #
-##################################
-
-
-@mark.parametrize(
-    "transformer",
-    (ToNumeric(), StripString(), {OBJECT_TARGET: "flamme.transformer.series.ToNumeric"}),
-)
-def test_numeric_column_get_transformer(transformer: BaseSeriesTransformer | dict) -> None:
-    assert isinstance(
-        NumericColumn(can_be_null=False, transformer=transformer).get_transformer(),
+        Column(
+            can_be_null=False,
+            analyzer=ColumnContinuousAnalyzer(column="col"),
+            transformer=transformer,
+        ).get_transformer(),
         BaseSeriesTransformer,
     )
-
-
-def test_numeric_column_get_transformer_default() -> None:
-    assert isinstance(NumericColumn(can_be_null=False).get_transformer(), ToNumeric)
-
-
-##################################
-#     Tests for StringColumn     #
-##################################
-
-
-@mark.parametrize(
-    "transformer",
-    (ToNumeric(), StripString(), {OBJECT_TARGET: "flamme.transformer.series.ToNumeric"}),
-)
-def test_string_column_get_transformer(transformer: BaseSeriesTransformer | dict) -> None:
-    assert isinstance(
-        StringColumn(can_be_null=False, transformer=transformer).get_transformer(),
-        BaseSeriesTransformer,
-    )
-
-
-def test_string_column_get_transformer_default() -> None:
-    assert isinstance(StringColumn(can_be_null=False).get_transformer(), StripString)
