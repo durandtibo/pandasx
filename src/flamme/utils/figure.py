2c1f2c92e50352630f1f73b0f0e385635ce784b4
Thibaut Durand <durand.tibo@gmail.com>
2023-12-19 07:28:08 -0800

    Add `figure2html` function

--- src/flamme/utils/figure.py
+++ src/flamme/utils/figure.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 __all__ = ["figure2html"]
 
 import base64
--- tests/unit/utils/test_figure.py
+++ tests/unit/utils/test_figure.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 __all__ = ["figure2html"]
 
 from matplotlib import pyplot as plt
