8eb7a64a7bc7a1d5ca4d396a5a6a447982b113c8
Thibaut Durand <durand.tibo@gmail.com>
2023-12-29 08:36:14 -0800

    Move `auto_yscale_continuous` to `utils` (#239)

--- tests/unit/section/test_continous.py
+++ tests/unit/section/test_continous.py
@@ -8,7 +8,6 @@ from pytest import fixture, mark
 
 from flamme.section import ColumnContinuousSection
 from flamme.section.continuous import (
-    auto_continuous_yscale,
     create_boxplot_figure,
     create_histogram_figure,
     create_stats_table,
@@ -313,50 +312,3 @@ def test_create_histogram_figure_figsize(
 
 def test_create_stats_table(stats: dict[str, float]) -> None:
     assert isinstance(create_stats_table(stats=stats, column="col"), str)
-
-
-#################################
-#     Tests for auto_yscale     #
-#################################
-
-
-@mark.parametrize("nbins", [1, 5, 10, 100, 1000])
-def test_auto_yscale_nbins(nbins: int) -> None:
-    assert auto_continuous_yscale(np.arange(100), nbins=nbins) == "linear"
-
-
-@mark.parametrize(
-    "array",
-    [
-        np.ones(100),
-        np.arange(100),
-        np.eye(10).flatten(),
-        np.asarray(list(range(100)) + [float("nan")]),
-    ],
-)
-def test_auto_yscale_linear(array: np.ndarray) -> None:
-    assert auto_continuous_yscale(array, nbins=10) == "linear"
-
-
-@mark.parametrize(
-    "array",
-    [
-        np.asarray([1] * 100 + list(range(1, 11))),
-        np.asarray([10] * 1000 + list(range(1, 11))),
-        np.asarray([1] * 100 + list(range(1, 11)) + [float("nan")]),
-    ],
-)
-def test_auto_yscale_log(array: np.ndarray) -> None:
-    assert auto_continuous_yscale(array, nbins=10) == "log"
-
-
-@mark.parametrize(
-    "array",
-    [
-        np.asarray([1] * 100 + [-1, 10, 100]),
-        np.asarray([100] * 1000 + [0, 10, 20]),
-        np.asarray([100] * 1000 + [-1, 10, 20, float("nan")]),
-    ],
-)
-def test_auto_yscale_symlog(array: np.ndarray) -> None:
-    assert auto_continuous_yscale(array, nbins=10) == "symlog"
