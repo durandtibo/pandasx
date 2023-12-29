8c0dc89eaaff41836ec0560527581f945db4d78d
Thibaut Durand <durand.tibo@gmail.com>
2023-12-29 12:44:57 -0800

    Add `NumUniqueSelection`

--- src/flamme/analyzer/choice.py
+++ src/flamme/analyzer/choice.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__all__ = ["ChoiceAnalyzer"]
+__all__ = ["ChoiceAnalyzer", "NumUniqueSelection"]
 
 from collections.abc import Callable, Mapping
 
@@ -76,3 +76,49 @@ class ChoiceAnalyzer(BaseAnalyzer):
     def analyze(self, df: DataFrame) -> BaseSection:
         analyzer = self._analyzers[self._selection_fn(df)]
         return analyzer.analyze(df)
+
+
+class NumUniqueSelection(Callable):
+    r"""Implements a selection logic based on the number of unique values
+    in a column.
+
+    Args:
+        column: Specifies the column to check.
+        threshold: Specifies the threshold of number of unique values.
+        small: Specifies the string to return if the number of unique
+            values is lower than the threshold.
+        large: Specifies the string to return if the number of unique
+            values is greater than the threshold.
+
+    Example usage:
+
+    ```pycon
+    >>> import numpy as np
+    >>> import pandas as pd
+    >>> from flamme.analyzer.choice import NumUniqueSelection
+    >>> selection = NumUniqueSelection(column='col')
+    >>> selection
+    NumUniqueSelection(column=col, threshold=100, small=small, large=large)
+    >>> selection(pd.DataFrame({'col': np.arange(10)}))
+    small
+    >>> selection(pd.DataFrame({'col': np.arange(1000)}))
+    large
+    """
+
+    def __init__(
+        self, column: str, threshold: int = 100, small: str = "small", large: str = "large"
+    ) -> None:
+        self._column = column
+        self._threshold = threshold
+        self._small = small
+        self._large = large
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__qualname__}(column={self._column}, threshold={self._threshold}, "
+            f"small={self._small}, large={self._large})"
+        )
+
+    def __call__(self, df: DataFrame) -> str:
+        nunique = df[self._column].nunique()
+        return self._small if nunique <= self._threshold else self._large
