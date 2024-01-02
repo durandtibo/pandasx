1768fa2058e5b9077dff74e8c6afe93f721ebf75
Thibaut Durand <durand.tibo@gmail.com>
2024-01-02 14:30:12 -0800

    Add `find_files`

--- tests/unit/utils/test_path.py
+++ tests/unit/utils/test_path.py
@@ -1,9 +1,19 @@
 from __future__ import annotations
 
+import tarfile
 from pathlib import Path
 from unittest.mock import Mock, patch
 
-from flamme.utils.path import human_file_size, sanitize_path
+import pytest
+from pandas import DataFrame
+
+from flamme.utils.io import save_text
+from flamme.utils.path import (
+    find_files,
+    find_parquet_files,
+    human_file_size,
+    sanitize_path,
+)
 
 #####################################
 #     Tests for human_file_size     #
@@ -45,3 +55,95 @@ def test_sanitize_path_resolve() -> None:
 
 def test_sanitize_path_uri() -> None:
     assert sanitize_path("file:///my/path/something/./../") == Path("/my/path")
+
+
+################################
+#     Tests for find_files     #
+################################
+
+
+@pytest.fixture(scope="module")
+def tar_path(tmp_path_factory: pytest.TempPathFactory) -> Path:
+    path = tmp_path_factory.mktemp("tmp")
+    save_text("text", path.joinpath("a.txt"))
+    path.joinpath("subfolder", "sub").mkdir(parents=True, exist_ok=True)
+    with tarfile.open(path.joinpath("data.tar"), "w") as tar:
+        tar.add(path.joinpath("a.txt"))
+    with tarfile.open(path.joinpath("data2.tar.gz"), "w:gz") as tar:
+        tar.add(path.joinpath("a.txt"))
+    with tarfile.open(path.joinpath("subfolder", "data.tar"), "w") as tar:
+        tar.add(path.joinpath("a.txt"))
+    with tarfile.open(path.joinpath("subfolder", "sub", "data.tar"), "w") as tar:
+        tar.add(path.joinpath("a.txt"))
+    return path
+
+
+def test_find_files(tar_path: Path) -> None:
+    assert sorted(find_files(tar_path, filter_fn=tarfile.is_tarfile)) == [
+        tar_path.joinpath("data.tar"),
+        tar_path.joinpath("data2.tar.gz"),
+        tar_path.joinpath("subfolder", "data.tar"),
+        tar_path.joinpath("subfolder", "sub", "data.tar"),
+    ]
+
+
+def test_find_files_recursive_false(tar_path: Path) -> None:
+    assert sorted(find_files(tar_path, filter_fn=tarfile.is_tarfile, recursive=False)) == [
+        tar_path.joinpath("data.tar"),
+        tar_path.joinpath("data2.tar.gz"),
+    ]
+
+
+def test_find_files_file(tar_path: Path) -> None:
+    assert find_files(
+        tar_path.joinpath("subfolder", "sub", "data.tar"), filter_fn=tarfile.is_tarfile
+    ) == [tar_path.joinpath("subfolder", "sub", "data.tar")]
+
+
+def test_find_files_empty(tmp_path: Path) -> None:
+    save_text("text", tmp_path.joinpath("file.txt"))
+    assert find_files(tmp_path, filter_fn=tarfile.is_tarfile) == []
+
+
+########################################
+#     Tests for find_parquet_files     #
+########################################
+
+
+@pytest.fixture(scope="module")
+def parquet_path(tmp_path_factory: pytest.TempPathFactory) -> Path:
+    path = tmp_path_factory.mktemp("tmp")
+    save_text("text", path.joinpath("a.txt"))
+    path.joinpath("subfolder", "sub").mkdir(parents=True, exist_ok=True)
+    DataFrame({}).to_parquet(path.joinpath("data.parquet"))
+    DataFrame({}).to_parquet(path.joinpath("data2.snappy.parquet"))
+    DataFrame({}).to_parquet(path.joinpath("subfolder", "data.parquet"))
+    DataFrame({}).to_parquet(path.joinpath("subfolder", "sub", "data.parquet"))
+    return path
+
+
+def test_find_parquet_files(parquet_path: Path) -> None:
+    assert sorted(find_parquet_files(parquet_path)) == [
+        parquet_path.joinpath("data.parquet"),
+        parquet_path.joinpath("data2.snappy.parquet"),
+        parquet_path.joinpath("subfolder", "data.parquet"),
+        parquet_path.joinpath("subfolder", "sub", "data.parquet"),
+    ]
+
+
+def test_find_parquet_files_recursive_false(parquet_path: Path) -> None:
+    assert sorted(find_parquet_files(parquet_path, recursive=False)) == [
+        parquet_path.joinpath("data.parquet"),
+        parquet_path.joinpath("data2.snappy.parquet"),
+    ]
+
+
+def test_find_parquet_files_file(parquet_path: Path) -> None:
+    assert find_parquet_files(parquet_path.joinpath("subfolder", "sub", "data.parquet")) == [
+        parquet_path.joinpath("subfolder", "sub", "data.parquet")
+    ]
+
+
+def test_find_parquet_files_empty(tmp_path: Path) -> None:
+    save_text("text", tmp_path.joinpath("file.txt"))
+    assert find_parquet_files(tmp_path) == []
