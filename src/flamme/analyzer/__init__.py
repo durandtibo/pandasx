6c028659c9762a465dd3d51ad3d03af5c18c1cff
Thibaut Durand <durand.tibo@gmail.com>
2024-01-04 11:29:00 -0800

    Add `DataFrameSummaryAnalyzer`

--- src/flamme/analyzer/__init__.py
+++ src/flamme/analyzer/__init__.py
@@ -10,6 +10,7 @@ __all__ = [
     "ColumnTemporalContinuousAnalyzer",
     "ColumnTemporalDiscreteAnalyzer",
     "ColumnTemporalNullValueAnalyzer",
+    "DataFrameSummaryAnalyzer",
     "DataTypeAnalyzer",
     "DuplicatedRowAnalyzer",
     "FilteredAnalyzer",
@@ -32,6 +33,7 @@ from flamme.analyzer.continuous import (
     ColumnTemporalContinuousAnalyzer,
 )
 from flamme.analyzer.continuous_advanced import ColumnContinuousAdvancedAnalyzer
+from flamme.analyzer.df_summary import DataFrameSummaryAnalyzer
 from flamme.analyzer.discrete import (
     ColumnDiscreteAnalyzer,
     ColumnTemporalDiscreteAnalyzer,
