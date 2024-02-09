7da9dcc1f6b29fd6dcf41b80716003f9f336f771
Thibaut Durand <durand.tibo@gmail.com>
2024-02-09 08:51:02 -0500

    Add `ClickHouseIngestor`

--- src/flamme/ingestor/__init__.py
+++ src/flamme/ingestor/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 __all__ = [
     "BaseIngestor",
+    "ClickHouseIngestor",
     "CsvIngestor",
     "Ingestor",
     "ParquetIngestor",
@@ -11,6 +12,7 @@ __all__ = [
 ]
 
 from flamme.ingestor.base import BaseIngestor, is_ingestor_config, setup_ingestor
+from flamme.ingestor.clickhouse import ClickHouseIngestor
 from flamme.ingestor.csv import CsvIngestor
 from flamme.ingestor.parquet import ParquetIngestor
 from flamme.ingestor.transform import TransformedIngestor
---
+++ tests/unit/ingestor/test_clickhouse.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+from unittest.mock import Mock, patch
+
+import pyarrow as pa
+from pandas import DataFrame
+from pandas.testing import assert_frame_equal
+
+from flamme.ingestor import ClickHouseIngestor
+from flamme.testing import clickhouse_connect_available
+
+########################################
+#     Tests for ClickHouseIngestor     #
+########################################
+
+
+@clickhouse_connect_available
+def test_clickhouse_ingestor_str() -> None:
+    assert str(
+        ClickHouseIngestor(query="select * from source.dataset", client_config={})
+    ).startswith("ClickHouseIngestor(")
+
+
+@clickhouse_connect_available
+def test_clickhouse_ingestor_ingest() -> None:
+    ingestor = ClickHouseIngestor(
+        query="select * from source.dataset",
+        client_config={"username": "POLAR", "password": "BEAR"},
+    )
+    df = DataFrame(
+        {
+            "col1": [1, 2, 3, 4, 5],
+            "col2": ["a", "b", "c", "d", "e"],
+            "col3": [1.2, 2.2, 3.2, 4.2, 5.2],
+        }
+    )
+    query_df_mock = Mock(return_value=pa.Table.from_pandas(df))
+    clickhouse_mock = Mock(return_value=Mock(query_df=query_df_mock))
+    with patch("flamme.ingestor.clickhouse.clickhouse_connect.get_client", clickhouse_mock):
+        df = ingestor.ingest()
+        assert_frame_equal(
+            df,
+            DataFrame(
+                {
+                    "col1": [1, 2, 3, 4, 5],
+                    "col2": ["a", "b", "c", "d", "e"],
+                    "col3": [1.2, 2.2, 3.2, 4.2, 5.2],
+                }
+            ),
+        )
+        clickhouse_mock.assert_called_once_with(username="POLAR", password="BEAR")
+        query_df_mock.assert_called_once_with(query="select * from source.dataset")
