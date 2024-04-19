26fa0791bb9b3ae39704e1a3c4cdc442595e722b
Thibaut Durand <durand.tibo@gmail.com>
2024-04-18 17:27:48 -0700

    Add vanilla schema reader

--- src/flamme/schema/reader/__init__.py
+++ src/flamme/schema/reader/__init__.py
@@ -4,9 +4,10 @@ from __future__ import annotations
 
 __all__ = [
     "BaseSchemaReader",
+    "ParquetSchemaReader",
+    "SchemaReader",
     "is_schema_reader_config",
     "setup_schema_reader",
-    "ParquetSchemaReader",
 ]
 
 from flamme.schema.reader.base import (
@@ -15,3 +16,4 @@ from flamme.schema.reader.base import (
     setup_schema_reader,
 )
 from flamme.schema.reader.parquet import ParquetSchemaReader
+from flamme.schema.reader.vanilla import SchemaReader
