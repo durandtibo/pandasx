7d461a5b9f2dfe095ec767c2db0a18f2475172dd
Thibaut Durand <durand.tibo@gmail.com>
2024-04-19 14:06:05 -0700

    Add `from_schema` to `ToNumericDataFrameTransformer` (#371)

--- tests/unit/transformer/df/test_numeric.py
+++ tests/unit/transformer/df/test_numeric.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import pandas as pd
+import pyarrow as pa
 from pandas.testing import assert_frame_equal
 
 from flamme.transformer.dataframe import ToNumeric
@@ -58,6 +59,59 @@ def test_to_numeric_dataframe_transformer_transform_kwargs() -> None:
         }
     )
     transformer = ToNumeric(columns=["col1", "col3"], errors="coerce")
+    out = transformer.transform(frame)
+    assert_frame_equal(
+        out,
+        pd.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": [1, 2, 3, 4, float("nan")],
+                "col4": ["a", "b", "c", "d", "e"],
+            }
+        ),
+    )
+
+
+def test_to_numeric_dataframe_transformer_from_schema() -> None:
+    frame = pd.DataFrame(
+        {
+            "col1": [1, 2, 3, 4, 5],
+            "col2": ["1", "2", "3", "4", "5"],
+            "col3": ["1", "2", "3", "4", "5"],
+            "col4": ["a", "b", "c", "d", "e"],
+        }
+    )
+    transformer = ToNumeric.from_schema(
+        schema=pa.schema([("col1", pa.int64()), ("col2", pa.string()), ("col3", pa.float64())])
+    )
+    out = transformer.transform(frame)
+    assert_frame_equal(
+        out,
+        pd.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": ["1", "2", "3", "4", "5"],
+                "col3": [1, 2, 3, 4, 5],
+                "col4": ["a", "b", "c", "d", "e"],
+            }
+        ),
+    )
+
+
+def test_to_numeric_dataframe_transformer_from_schema_kwargs() -> None:
+    frame = pd.DataFrame(
+        {
+            "col1": [1, 2, 3, 4, 5],
+            "col2": ["1", "2", "3", "4", "5"],
+            "col3": ["1", "2", "3", "4", "a5"],
+            "col4": ["a", "b", "c", "d", "e"],
+        }
+    )
+    transformer = ToNumeric.from_schema(
+        schema=pa.schema([("col1", pa.int64()), ("col2", pa.string()), ("col3", pa.float64())]),
+        errors="coerce",
+    )
     out = transformer.transform(frame)
     assert_frame_equal(
         out,
