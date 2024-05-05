b0f649dc756639cdd4c576e9950c59cb3e632c3b
Thibaut Durand <durand.tibo@gmail.com>
2024-05-04 23:27:55 -0700

    Make figures reactive

--- src/flamme/utils/figure.py
+++ src/flamme/utils/figure.py
@@ -15,12 +15,14 @@ if TYPE_CHECKING:
     from matplotlib.axes import Axes
 
 
-def figure2html(fig: plt.Figure, close_fig: bool = False) -> str:
+def figure2html(fig: plt.Figure, reactive: bool = True, close_fig: bool = False) -> str:
     r"""Convert a matplotlib figure to a string that can be used in a
     HTML file.
 
     Args:
         fig: The figure to convert.
+        reactive: If ``True``, the generated is configured to be
+            reactive to the screen size.
         close_fig: If ``True``, the figure is closed after it is
             converted to HTML format.
 
@@ -44,7 +46,8 @@ def figure2html(fig: plt.Figure, close_fig: bool = False) -> str:
     data = base64.b64encode(img.getvalue()).decode("utf-8")
     if close_fig:
         plt.close(fig)
-    return f'<img src="data:image/png;charset=utf-8;base64, {data}">'
+    style = 'style="width:100%; height:auto;" ' if reactive else False
+    return f'<img {style}src="data:image/png;charset=utf-8;base64, {data}">'
 
 
 def readable_xticklabels(
