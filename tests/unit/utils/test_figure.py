9e27c81c2563cceefc003270f6a3b0a0fb9b2829
Thibaut Durand <durand.tibo@gmail.com>
2024-05-04 23:33:05 -0700

    Make figures reactive (#396)

--- tests/unit/utils/test_figure.py
+++ tests/unit/utils/test_figure.py
@@ -31,9 +31,16 @@ def test_figure2html() -> None:
     assert isinstance(figure2html(fig), str)
 
 
-def test_figure2html_close_fig() -> None:
+@pytest.mark.parametrize("close_fig", [True, False])
+def test_figure2html_close_fig(close_fig: bool) -> None:
     fig, _ = plt.subplots()
-    assert isinstance(figure2html(fig, close_fig=True), str)
+    assert isinstance(figure2html(fig, close_fig=close_fig), str)
+
+
+@pytest.mark.parametrize("reactive", [True, False])
+def test_figure2html_reactive(reactive: bool) -> None:
+    fig, _ = plt.subplots()
+    assert isinstance(figure2html(fig, reactive=reactive), str)
 
 
 ##########################################
