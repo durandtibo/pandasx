b1406bc432d2ec8bf50480a5ba1cbaf770d69980
Thibaut Durand <durand.tibo@gmail.com>
2024-05-04 21:51:29 -0700

    Improve demo scripts (#394)

--- scripts/demo_large.py
+++ scripts/demo_large.py
@@ -13,10 +13,7 @@ from flamme.analyzer import (
     BaseAnalyzer,
     ColumnContinuousAdvancedAnalyzer,
     ColumnContinuousAnalyzer,
-    ColumnDiscreteAnalyzer,
     ColumnTemporalContinuousAnalyzer,
-    ColumnTemporalDiscreteAnalyzer,
-    ColumnTemporalNullValueAnalyzer,
     DataFrameSummaryAnalyzer,
     DataTypeAnalyzer,
     DuplicatedRowAnalyzer,
@@ -31,11 +28,7 @@ from flamme.ingestor import Ingestor
 from flamme.reporter import BaseReporter, Reporter
 from flamme.transformer.dataframe import (
     BaseDataFrameTransformer,
-    ColumnSelection,
     SequentialDataFrameTransformer,
-    StripString,
-    ToDatetime,
-    ToNumeric,
 )
 from flamme.utils.logging import configure_logging
 
@@ -44,36 +37,24 @@ logger = logging.getLogger(__name__)
 FIGSIZE = (14, 5)
 
 
-def create_dataframe(nrows: int = 1000) -> pd.DataFrame:
+def create_dataframe(nrows: int = 1000, ncols: int = 100) -> pd.DataFrame:
     r"""Create a DataFrame.
 
     Args:
         nrows: Specifies the number of rows.
+        ncols: Specifies the number of columns.
 
     Returns:
-        The generated DayaFrame.
+        The generated DataFrame.
     """
     rng = np.random.default_rng(42)
     frame = pd.DataFrame(
-        {
-            "bool": np.random.randint(0, 2, (nrows,), dtype=bool),
-            "float": np.random.randn(nrows) * 3 + 1,
-            "int": np.random.randint(0, 10, (nrows,)),
-            "str": np.random.choice(["A", "B", "C"], size=(nrows,), p=[0.6, 0.3, 0.1]),
-            "cauchy": rng.standard_cauchy(size=(nrows,)),
-            "half cauchy": np.abs(rng.standard_cauchy(size=(nrows,))),
-        }
+        rng.normal(size=(nrows, ncols)), columns=[f"feature{i:04}" for i in range(ncols)]
     )
     mask = rng.choice([True, False], size=frame.shape, p=[0.2, 0.8])
-    mask[:, 0] = rng.choice([True, False], size=(mask.shape[0]), p=[0.4, 0.6])
-    mask[:, 1] = rng.choice([True, False], size=(mask.shape[0]), p=[0.8, 0.2])
-    mask[:, 2] = rng.choice([True, False], size=(mask.shape[0]), p=[0.6, 0.4])
-    mask[:, 3] = rng.choice([True, False], size=(mask.shape[0]), p=[0.2, 0.8])
     mask[mask.all(1), -1] = 0
     frame = frame.mask(mask)
-    frame["discrete"] = np.random.randint(0, 1001, (nrows,))
     frame["datetime"] = pd.date_range("2018-01-01", periods=nrows, freq="H")
-    frame["datetime_str"] = pd.date_range("2018-01-01", periods=nrows, freq="H").astype(str)
     return frame
 
 
@@ -94,35 +75,6 @@ def create_null_value_analyzer() -> BaseAnalyzer:
             "daily": TemporalNullValueAnalyzer(
                 dt_column="datetime", period="D", ncols=1, figsize=FIGSIZE
             ),
-        }
-    )
-
-
-def create_discrete_column_analyzer(column: str) -> BaseAnalyzer:
-    r"""Instantiate an analyzer about discrete values for a given column.
-
-    Args:
-        column: Specifies the column to analyze.
-
-    Returns:
-        The instantiated analyzer.
-    """
-    return MappingAnalyzer(
-        {
-            "overall": ColumnDiscreteAnalyzer(column=column, figsize=FIGSIZE),
-            "monthly": ColumnTemporalDiscreteAnalyzer(
-                column=column, dt_column="datetime", period="M", figsize=FIGSIZE
-            ),
-            # "weekly": ColumnTemporalDiscreteAnalyzer(
-            #     column=column, dt_column="datetime", period="W", figsize=FIGSIZE
-            # ),
-            # "daily": ColumnTemporalDiscreteAnalyzer(
-            #     column=column, dt_column="datetime", period="D", figsize=FIGSIZE
-            # ),
-            "null monthly": ColumnTemporalNullValueAnalyzer(
-                column=column, dt_column="datetime", period="M", figsize=FIGSIZE
-            ),
-            "most frequent": MostFrequentValuesAnalyzer(column=column, top=10),
         }
     )
 
@@ -193,22 +145,7 @@ def create_analyzer() -> BaseAnalyzer:
     Returns:
         The instantiated analyzer.
     """
-    columns = MappingAnalyzer(
-        {
-            "str": create_discrete_column_analyzer(column="str"),
-            "int": create_discrete_column_analyzer(column="int"),
-            "discrete": create_discrete_column_analyzer(column="discrete"),
-            "missing": ColumnDiscreteAnalyzer(column="missing"),
-            "float": create_continuous_column_analyzer(column="float"),
-            "cauchy": create_continuous_column_analyzer(
-                column="cauchy", yscale="auto", xmin="g0.001", xmax="g0.999"
-            ),
-            "half cauchy": create_continuous_column_analyzer(
-                column="half cauchy", yscale="log", xmax="q0.99"
-            ),
-        }
-    )
-    # columns = MappingAnalyzer({})
+    columns = MappingAnalyzer({})
     return MappingAnalyzer(
         {
             "summary": DataFrameSummaryAnalyzer(),
@@ -221,9 +158,6 @@ def create_analyzer() -> BaseAnalyzer:
             "column type": DataTypeAnalyzer(),
             "null values": create_null_value_analyzer(),
             "columns": columns,
-            # "subset": ColumnSubsetAnalyzer(
-            #     columns=["discrete", "str"], analyzer=NullValueAnalyzer()
-            # ),
         }
     )
 
@@ -234,17 +168,7 @@ def create_transformer() -> BaseDataFrameTransformer:
     Returns:
         The instantiated transformer.
     """
-    return SequentialDataFrameTransformer(
-        [
-            ColumnSelection(
-                columns=["str", "float", "int", "cauchy", "datetime", "datetime_str", "missing"],
-                ignore_missing=True,
-            ),
-            StripString(columns=["str"]),
-            ToNumeric(columns=["float", "int", "cauchy"]),
-            ToDatetime(columns=["datetime", "datetime_str"]),
-        ]
-    )
+    return SequentialDataFrameTransformer([])
 
 
 def create_reporter() -> BaseReporter:
@@ -254,10 +178,10 @@ def create_reporter() -> BaseReporter:
         The instantiated reporter.
     """
     return Reporter(
-        ingestor=Ingestor(frame=create_dataframe(nrows=10000)),
+        ingestor=Ingestor(frame=create_dataframe(nrows=10000, ncols=500)),
         transformer=create_transformer(),
         analyzer=create_analyzer(),
-        report_path=Path.cwd().joinpath("tmp/report.html"),
+        report_path=Path.cwd().joinpath("tmp/report_large.html"),
     )
 
 
