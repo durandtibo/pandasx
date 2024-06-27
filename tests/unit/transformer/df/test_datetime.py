8e6f69b8638710abf26b81ea7639c37cac2030c3
Thibaut Durand <durand.tibo@gmail.com>
2024-06-27 08:52:55 -0700

    Improve `ToDatetimeDataFrameTransformer` (#463)

--- tests/unit/transformer/df/test_datetime.py
+++ tests/unit/transformer/df/test_datetime.py
@@ -1,10 +1,26 @@
 from __future__ import annotations
 
+import logging
+
 import pandas as pd
 import pyarrow as pa
+import pytest
 from pandas.testing import assert_frame_equal
 
 from flamme.transformer.dataframe import ToDatetime
+
+
+@pytest.fixture()
+def dataframe() -> pd.DataFrame:
+    return pd.DataFrame(
+        {
+            "col1": ["2020-1-1", "2020-1-2", "2020-1-31", "2020-12-31", "2021-12-31"],
+            "col2": [1, 2, 3, 4, 5],
+            "col3": ["a", "b", "c", "d", "e"],
+            "col4": ["2020-1-1", "2020-1-2", "2020-1-31", "2020-12-31", "2021-12-31"],
+        }
+    )
+
 
 ####################################################
 #     Tests for ToDatetimeDataFrameTransformer     #
@@ -14,28 +30,20 @@ from flamme.transformer.dataframe import ToDatetime
 def test_to_datetime_dataframe_transformer_str() -> None:
     assert (
         str(ToDatetime(columns=["col1", "col3"]))
-        == "ToDatetimeDataFrameTransformer(columns=('col1', 'col3'))"
+        == "ToDatetimeDataFrameTransformer(columns=('col1', 'col3'), ignore_missing=False)"
     )
 
 
 def test_to_datetime_dataframe_transformer_str_kwargs() -> None:
-    assert (
-        str(ToDatetime(columns=["col1", "col3"], errors="ignore"))
-        == "ToDatetimeDataFrameTransformer(columns=('col1', 'col3'), errors=ignore)"
+    assert str(ToDatetime(columns=["col1", "col3"], errors="ignore")) == (
+        "ToDatetimeDataFrameTransformer(columns=('col1', 'col3'), "
+        "ignore_missing=False, errors=ignore)"
     )
 
 
-def test_to_datetime_dataframe_transformer_transform() -> None:
-    frame = pd.DataFrame(
-        {
-            "col1": ["2020-1-1", "2020-1-2", "2020-1-31", "2020-12-31", "2021-12-31"],
-            "col2": [1, 2, 3, 4, 5],
-            "col3": ["a", "b", "c", "d", "e"],
-            "col4": ["2020-1-1", "2020-1-2", "2020-1-31", "2020-12-31", "2021-12-31"],
-        }
-    )
+def test_to_datetime_dataframe_transformer_transform(dataframe: pd.DataFrame) -> None:
     transformer = ToDatetime(columns=["col1"])
-    out = transformer.transform(frame)
+    out = transformer.transform(dataframe)
     assert_frame_equal(
         out,
         pd.DataFrame(
@@ -52,7 +60,7 @@ def test_to_datetime_dataframe_transformer_transform() -> None:
 
 
 def test_to_datetime_dataframe_transformer_transform_kwargs() -> None:
-    frame = pd.DataFrame(
+    dataframe = pd.DataFrame(
         {
             "col1": ["2020-1-1", "2020-1-2", "2020-1-31", "2020-12-31", "abc"],
             "col2": [1, 2, 3, 4, 5],
@@ -61,7 +69,7 @@ def test_to_datetime_dataframe_transformer_transform_kwargs() -> None:
         }
     )
     transformer = ToDatetime(columns=["col1", "col3"], errors="coerce", format="%Y-%m-%d")
-    out = transformer.transform(frame)
+    out = transformer.transform(dataframe)
     assert_frame_equal(
         out,
         pd.DataFrame(
@@ -78,19 +86,43 @@ def test_to_datetime_dataframe_transformer_transform_kwargs() -> None:
     )
 
 
-def test_to_datetime_dataframe_transformer_from_schema() -> None:
-    frame = pd.DataFrame(
-        {
-            "col1": ["2020-1-1", "2020-1-2", "2020-1-31", "2020-12-31", "2021-12-31"],
-            "col2": [1, 2, 3, 4, 5],
-            "col3": ["a", "b", "c", "d", "e"],
-            "col4": ["2020-1-1", "2020-1-2", "2020-1-31", "2020-12-31", "2021-12-31"],
-        }
-    )
+def test_to_datetime_dataframe_transformer_transform_ignore_missing_false(
+    dataframe: pd.DataFrame,
+) -> None:
+    transformer = ToDatetime(columns=["col1", "col5"])
+    with pytest.raises(RuntimeError, match="column col5 is not in the DataFrame"):
+        transformer.transform(dataframe)
+
+
+def test_to_datetime_dataframe_transformer_transform_ignore_missing_true(
+    dataframe: pd.DataFrame, caplog: pytest.LogCaptureFixture
+) -> None:
+    transformer = ToDatetime(columns=["col1", "col5"], ignore_missing=True)
+    with caplog.at_level(logging.WARNING):
+        out = transformer.transform(dataframe)
+        assert_frame_equal(
+            out,
+            pd.DataFrame(
+                {
+                    "col1": pd.to_datetime(
+                        ["2020-1-1", "2020-1-2", "2020-1-31", "2020-12-31", "2021-12-31"]
+                    ),
+                    "col2": [1, 2, 3, 4, 5],
+                    "col3": ["a", "b", "c", "d", "e"],
+                    "col4": ["2020-1-1", "2020-1-2", "2020-1-31", "2020-12-31", "2021-12-31"],
+                }
+            ),
+        )
+        assert caplog.messages[-1].startswith(
+            "skipping transformation for column col5 because the column is missing"
+        )
+
+
+def test_to_datetime_dataframe_transformer_from_schema(dataframe: pd.DataFrame) -> None:
     transformer = ToDatetime.from_schema(
         schema=pa.schema([("col1", pa.date64()), ("col2", pa.string()), ("col3", pa.int64())])
     )
-    out = transformer.transform(frame)
+    out = transformer.transform(dataframe)
     assert_frame_equal(
         out,
         pd.DataFrame(
@@ -107,7 +139,7 @@ def test_to_datetime_dataframe_transformer_from_schema() -> None:
 
 
 def test_to_datetime_dataframe_transformer_from_schema_kwargs() -> None:
-    frame = pd.DataFrame(
+    dataframe = pd.DataFrame(
         {
             "col1": ["2020-1-1", "2020-1-2", "2020-1-31", "2020-12-31", "abc"],
             "col2": [1, 2, 3, 4, 5],
@@ -120,7 +152,7 @@ def test_to_datetime_dataframe_transformer_from_schema_kwargs() -> None:
         errors="coerce",
         format="%Y-%m-%d",
     )
-    out = transformer.transform(frame)
+    out = transformer.transform(dataframe)
     assert_frame_equal(
         out,
         pd.DataFrame(
@@ -135,3 +167,37 @@ def test_to_datetime_dataframe_transformer_from_schema_kwargs() -> None:
             }
         ),
     )
+
+
+def test_to_datetime_dataframe_transformer_from_schema_ignore_missing(
+    dataframe: pd.DataFrame, caplog: pytest.LogCaptureFixture
+) -> None:
+    transformer = ToDatetime.from_schema(
+        schema=pa.schema(
+            [
+                ("col1", pa.date64()),
+                ("col2", pa.string()),
+                ("col3", pa.int64()),
+                ("col5", pa.date64()),
+            ]
+        ),
+        ignore_missing=True,
+    )
+    with caplog.at_level(logging.WARNING):
+        out = transformer.transform(dataframe)
+        assert_frame_equal(
+            out,
+            pd.DataFrame(
+                {
+                    "col1": pd.to_datetime(
+                        ["2020-1-1", "2020-1-2", "2020-1-31", "2020-12-31", "2021-12-31"]
+                    ),
+                    "col2": [1, 2, 3, 4, 5],
+                    "col3": ["a", "b", "c", "d", "e"],
+                    "col4": ["2020-1-1", "2020-1-2", "2020-1-31", "2020-12-31", "2021-12-31"],
+                }
+            ),
+        )
+        assert caplog.messages[-1].startswith(
+            "skipping transformation for column col5 because the column is missing"
+        )
