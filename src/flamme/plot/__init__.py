052d5c4159cb3dc32abb2db741576ac2359d3b8f
Thibaut Durand <durand.tibo@gmail.com>
2024-07-04 14:56:05 -0700

    Add `plot_null_temporal`

---
+++ src/flamme/plot/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain plotting functionalities."""
+
+from __future__ import annotations
+
+__all__ = ["plot_null_temporal"]
+
+from flamme.plot.null_temp import plot_null_temporal
---
+++ tests/unit/plot/test_null_temp.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import pytest
+from matplotlib import pyplot as plt
+
+from flamme.plot import plot_null_temporal
+
+########################################
+#     Tests for plot_null_temporal     #
+########################################
+
+
+def test_plot_null_temporal() -> None:
+    fig, ax = plt.subplots()
+    plot_null_temporal(
+        ax, nulls=[1, 2, 3, 4], totals=[10, 12, 14, 16], labels=["jan", "feb", "mar", "apr"]
+    )
+
+
+def test_plot_null_temporal_empty() -> None:
+    fig, ax = plt.subplots()
+    plot_null_temporal(ax, nulls=[], totals=[], labels=[])
+
+
+def test_plot_null_temporal_incorrect_total() -> None:
+    fig, ax = plt.subplots()
+    with pytest.raises(RuntimeError, match="nulls .* and totals .* have different lengths"):
+        plot_null_temporal(
+            ax, nulls=[1, 2, 3, 4], totals=[10, 12, 14, 16, 18], labels=["jan", "feb", "mar", "apr"]
+        )
+
+
+def test_plot_null_temporal_incorrect_labels() -> None:
+    fig, ax = plt.subplots()
+    with pytest.raises(RuntimeError, match="nulls .* and labels .* have different lengths"):
+        plot_null_temporal(
+            ax,
+            nulls=[1, 2, 3, 4],
+            totals=[10, 12, 14, 16],
+            labels=["jan", "feb", "mar", "apr", "may"],
+        )
