e1a065325414fa3df7cdf7a7845351e85bfddab6
Thibaut Durand <durand.tibo@gmail.com>
2024-07-06 13:02:33 -0700

    Improve casting dataframe transformers (#503)

--- tests/unit/transformer/dataframe/test_casting.py
+++ tests/unit/transformer/dataframe/test_casting.py
@@ -1,25 +1,18 @@
 from __future__ import annotations
 
+import logging
 from datetime import time
 
 import polars as pl
+import pytest
 from polars.testing import assert_frame_equal
 
 from flamme.transformer.dataframe import Cast, ToTime
 
-##############################################
-#     Tests for CastDataFrameTransformer     #
-##############################################
 
-
-def test_cast_dataframe_transformer_str() -> None:
-    assert str(Cast(columns=["col1", "col3"], dtype=pl.Int32)).startswith(
-        "CastDataFrameTransformer("
-    )
-
-
-def test_cast_dataframe_transformer_transform_int32() -> None:
-    frame = pl.DataFrame(
+@pytest.fixture()
+def dataframe() -> pl.DataFrame:
+    return pl.DataFrame(
         {
             "col1": [1, 2, 3, 4, 5],
             "col2": ["1", "2", "3", "4", "5"],
@@ -28,8 +21,42 @@ def test_cast_dataframe_transformer_transform_int32() -> None:
         },
         schema={"col1": pl.Int64, "col2": pl.String, "col3": pl.String, "col4": pl.String},
     )
+
+
+##############################################
+#     Tests for CastDataFrameTransformer     #
+##############################################
+
+
+def test_cast_dataframe_transformer_repr() -> None:
+    assert repr(Cast(columns=["col1", "col3"], dtype=pl.Int32)) == (
+        "CastDataFrameTransformer(columns=('col1', 'col3'), dtype=Int32, ignore_missing=False)"
+    )
+
+
+def test_cast_dataframe_transformer_repr_with_kwargs() -> None:
+    assert repr(Cast(columns=["col1", "col3"], dtype=pl.Int32, strict=False)) == (
+        "CastDataFrameTransformer(columns=('col1', 'col3'), dtype=Int32, ignore_missing=False, "
+        "strict=False)"
+    )
+
+
+def test_cast_dataframe_transformer_str() -> None:
+    assert str(Cast(columns=["col1", "col3"], dtype=pl.Int32)) == (
+        "CastDataFrameTransformer(columns=('col1', 'col3'), dtype=Int32, ignore_missing=False)"
+    )
+
+
+def test_cast_dataframe_transformer_str_with_kwargs() -> None:
+    assert str(Cast(columns=["col1", "col3"], dtype=pl.Int32, strict=False)) == (
+        "CastDataFrameTransformer(columns=('col1', 'col3'), dtype=Int32, ignore_missing=False, "
+        "strict=False)"
+    )
+
+
+def test_cast_dataframe_transformer_transform_int32(dataframe: pl.DataFrame) -> None:
     transformer = Cast(columns=["col1", "col3"], dtype=pl.Int32)
-    out = transformer.transform(frame)
+    out = transformer.transform(dataframe)
     assert_frame_equal(
         out,
         pl.DataFrame(
@@ -44,18 +71,9 @@ def test_cast_dataframe_transformer_transform_int32() -> None:
     )
 
 
-def test_cast_dataframe_transformer_transform_float32() -> None:
-    frame = pl.DataFrame(
-        {
-            "col1": [1, 2, 3, 4, 5],
-            "col2": ["1", "2", "3", "4", "5"],
-            "col3": ["1", "2", "3", "4", "5"],
-            "col4": ["a", "b", "c", "d", "e"],
-        },
-        schema={"col1": pl.Int64, "col2": pl.String, "col3": pl.String, "col4": pl.String},
-    )
+def test_cast_dataframe_transformer_transform_float32(dataframe: pl.DataFrame) -> None:
     transformer = Cast(columns=["col1", "col2"], dtype=pl.Float32)
-    out = transformer.transform(frame)
+    out = transformer.transform(dataframe)
     assert_frame_equal(
         out,
         pl.DataFrame(
@@ -70,13 +88,71 @@ def test_cast_dataframe_transformer_transform_float32() -> None:
     )
 
 
+def test_cast_dataframe_transformer_transform_ignore_missing_false(
+    dataframe: pl.DataFrame,
+) -> None:
+    transformer = Cast(columns=["col1", "col3", "col5"], dtype=pl.Float32)
+    with pytest.raises(RuntimeError, match="column col5 is not in the DataFrame"):
+        transformer.transform(dataframe)
+
+
+def test_cast_dataframe_transformer_transform_ignore_missing_true(
+    dataframe: pl.DataFrame, caplog: pytest.LogCaptureFixture
+) -> None:
+    transformer = Cast(columns=["col1", "col3", "col5"], dtype=pl.Float32, ignore_missing=True)
+    with caplog.at_level(logging.WARNING):
+        out = transformer.transform(dataframe)
+        assert_frame_equal(
+            out,
+            pl.DataFrame(
+                {
+                    "col1": [1.0, 2.0, 3.0, 4.0, 5.0],
+                    "col2": ["1", "2", "3", "4", "5"],
+                    "col3": [1.0, 2.0, 3.0, 4.0, 5.0],
+                    "col4": ["a", "b", "c", "d", "e"],
+                },
+                schema={
+                    "col1": pl.Float32,
+                    "col2": pl.String,
+                    "col3": pl.Float32,
+                    "col4": pl.String,
+                },
+            ),
+        )
+        assert caplog.messages[-1].startswith(
+            "skipping transformation for column col5 because the column is missing"
+        )
+
+
 ################################################
 #     Tests for ToTimeDataFrameTransformer     #
 ################################################
 
 
+def test_to_time_dataframe_transformer_repr() -> None:
+    assert repr(ToTime(columns=["col1", "col3"])) == (
+        "ToTimeDataFrameTransformer(columns=('col1', 'col3'), format=None, ignore_missing=False)"
+    )
+
+
+def test_to_time_dataframe_transformer_repr_with_kwargs() -> None:
+    assert repr(ToTime(columns=["col1", "col3"], strict=False)) == (
+        "ToTimeDataFrameTransformer(columns=('col1', 'col3'), format=None, ignore_missing=False, "
+        "strict=False)"
+    )
+
+
 def test_to_time_dataframe_transformer_str() -> None:
-    assert str(ToTime(columns=["col1", "col3"])).startswith("ToTimeDataFrameTransformer(")
+    assert str(ToTime(columns=["col1", "col3"])) == (
+        "ToTimeDataFrameTransformer(columns=('col1', 'col3'), format=None, ignore_missing=False)"
+    )
+
+
+def test_to_time_dataframe_transformer_str_with_kwargs() -> None:
+    assert str(ToTime(columns=["col1", "col3"], strict=False)) == (
+        "ToTimeDataFrameTransformer(columns=('col1', 'col3'), format=None, ignore_missing=False, "
+        "strict=False)"
+    )
 
 
 def test_to_time_dataframe_transformer_transform_no_format() -> None:
@@ -153,3 +229,62 @@ def test_to_time_dataframe_transformer_transform_format() -> None:
             schema={"col1": pl.Time, "col2": pl.String, "col3": pl.Time, "col4": pl.String},
         ),
     )
+
+
+def test_to_time_dataframe_transformer_transform_ignore_missing_false() -> None:
+    frame = pl.DataFrame(
+        {
+            "col1": ["01:01:01", "02:02:02", "12:00:01", "18:18:18", "23:59:59"],
+            "col2": ["1", "2", "3", "4", "5"],
+            "col3": ["11:11:11", "12:12:12", "13:13:13", "08:08:08", "23:59:59"],
+            "col4": ["01:01:01", "02:02:02", "12:00:01", "18:18:18", "23:59:59"],
+        },
+        schema={"col1": pl.String, "col2": pl.String, "col3": pl.String, "col4": pl.String},
+    )
+    transformer = ToTime(columns=["col1", "col3", "col5"], format="%H:%M:%S")
+    with pytest.raises(RuntimeError, match="column col5 is not in the DataFrame"):
+        transformer.transform(frame)
+
+
+def test_to_time_dataframe_transformer_transform_ignore_missing_true(
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    frame = pl.DataFrame(
+        {
+            "col1": ["01:01:01", "02:02:02", "12:00:01", "18:18:18", "23:59:59"],
+            "col2": ["1", "2", "3", "4", "5"],
+            "col3": ["11:11:11", "12:12:12", "13:13:13", "08:08:08", "23:59:59"],
+            "col4": ["01:01:01", "02:02:02", "12:00:01", "18:18:18", "23:59:59"],
+        },
+        schema={"col1": pl.String, "col2": pl.String, "col3": pl.String, "col4": pl.String},
+    )
+    transformer = ToTime(columns=["col1", "col3", "col5"], format="%H:%M:%S", ignore_missing=True)
+    with caplog.at_level(logging.WARNING):
+        out = transformer.transform(frame)
+        assert_frame_equal(
+            out,
+            pl.DataFrame(
+                {
+                    "col1": [
+                        time(1, 1, 1),
+                        time(2, 2, 2),
+                        time(12, 0, 1),
+                        time(18, 18, 18),
+                        time(23, 59, 59),
+                    ],
+                    "col2": ["1", "2", "3", "4", "5"],
+                    "col3": [
+                        time(11, 11, 11),
+                        time(12, 12, 12),
+                        time(13, 13, 13),
+                        time(8, 8, 8),
+                        time(23, 59, 59),
+                    ],
+                    "col4": ["01:01:01", "02:02:02", "12:00:01", "18:18:18", "23:59:59"],
+                },
+                schema={"col1": pl.Time, "col2": pl.String, "col3": pl.Time, "col4": pl.String},
+            ),
+        )
+        assert caplog.messages[-1].startswith(
+            "skipping transformation for column col5 because the column is missing"
+        )
