ac3292ef5bf0ee84adced54376a2c6463aeb41b7
Thibaut Durand <durand.tibo@gmail.com>
2024-07-06 08:57:32 -0700

    Update `Ingestor` (#494)

--- tests/unit/ingestor/test_vanilla.py
+++ tests/unit/ingestor/test_vanilla.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 import pandas as pd
-from pandas.testing import assert_frame_equal
+import polars as pl
+from polars.testing import assert_frame_equal
 
 from flamme.ingestor import Ingestor
 
@@ -24,18 +25,41 @@ def test_ingestor_str() -> None:
     ).startswith("Ingestor(")
 
 
-def test_ingestor_ingest() -> None:
+def test_ingestor_ingest_polars() -> None:
+    frame = pl.DataFrame(
+        {
+            "col1": [1, 2, 3, 4, 5],
+            "col2": ["a", "b", "c", "d", "e"],
+            "col3": [1.2, 2.2, 3.2, 4.2, 5.2],
+        }
+    )
+    out = Ingestor(frame=frame).ingest()
+    assert frame is out
     assert_frame_equal(
-        Ingestor(
-            frame=pd.DataFrame(
-                {
-                    "col1": [1, 2, 3, 4, 5],
-                    "col2": ["a", "b", "c", "d", "e"],
-                    "col3": [1.2, 2.2, 3.2, 4.2, 5.2],
-                }
-            )
-        ).ingest(),
-        pd.DataFrame(
+        out,
+        pl.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": ["a", "b", "c", "d", "e"],
+                "col3": [1.2, 2.2, 3.2, 4.2, 5.2],
+            }
+        ),
+    )
+
+
+def test_ingestor_ingest_pandas() -> None:
+    frame = pd.DataFrame(
+        {
+            "col1": [1, 2, 3, 4, 5],
+            "col2": ["a", "b", "c", "d", "e"],
+            "col3": [1.2, 2.2, 3.2, 4.2, 5.2],
+        }
+    )
+    out = Ingestor(frame=frame).ingest()
+    assert frame is not out
+    assert_frame_equal(
+        out,
+        pl.DataFrame(
             {
                 "col1": [1, 2, 3, 4, 5],
                 "col2": ["a", "b", "c", "d", "e"],
