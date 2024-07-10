5cd402ab5145d1a7f8d1e5dd06639dcaa9375b5e
Thibaut Durand <durand.tibo@gmail.com>
2024-07-09 23:24:34 -0700

    Make `markdown` optional (#519)

--- tests/unit/analyzer/test_markdown.py
+++ tests/unit/analyzer/test_markdown.py
@@ -5,16 +5,19 @@ import pandas as pd
 
 from flamme.analyzer import MarkdownAnalyzer
 from flamme.section import MarkdownSection
+from flamme.testing import markdown_available
 
 ######################################
 #     Tests for MarkdownAnalyzer     #
 ######################################
 
 
+@markdown_available
 def test_markdown_analyzer_str() -> None:
     assert str(MarkdownAnalyzer(desc="hello cats!")).startswith("MarkdownAnalyzer(")
 
 
+@markdown_available
 def test_markdown_analyzer_get_statistics() -> None:
     section = MarkdownAnalyzer(desc="hello cats!").analyze(
         pd.DataFrame(
@@ -29,6 +32,7 @@ def test_markdown_analyzer_get_statistics() -> None:
     assert section.get_statistics() == {}
 
 
+@markdown_available
 def test_markdown_analyzer_get_statistics_empty() -> None:
     section = MarkdownAnalyzer(desc="hello cats!").analyze(pd.DataFrame({}))
     assert isinstance(section, MarkdownSection)
--- tests/unit/section/test_markdown.py
+++ tests/unit/section/test_markdown.py
@@ -3,31 +3,37 @@ from __future__ import annotations
 from jinja2 import Template
 
 from flamme.section import MarkdownSection
+from flamme.testing import markdown_available
 
 #####################################
 #     Tests for MarkdownSection     #
 #####################################
 
 
+@markdown_available
 def test_markdown_section_str() -> None:
     assert str(MarkdownSection(desc="meow")).startswith("MarkdownSection(")
 
 
+@markdown_available
 def test_markdown_section_get_statistics() -> None:
     section = MarkdownSection(desc="### Hello Cat!")
     assert section.get_statistics() == {}
 
 
+@markdown_available
 def test_markdown_section_get_statistics_empty() -> None:
     section = MarkdownSection(desc="")
     assert section.get_statistics() == {}
 
 
+@markdown_available
 def test_markdown_section_render_html_body() -> None:
     section = MarkdownSection(desc="### Hello Cat!")
     assert isinstance(Template(section.render_html_body()).render(), str)
 
 
+@markdown_available
 def test_markdown_section_render_html_body_args() -> None:
     section = MarkdownSection(desc="### Hello Cat!")
     assert isinstance(
@@ -35,16 +41,19 @@ def test_markdown_section_render_html_body_args() -> None:
     )
 
 
+@markdown_available
 def test_markdown_section_render_html_body_empty() -> None:
     section = MarkdownSection(desc="")
     assert isinstance(Template(section.render_html_body()).render(), str)
 
 
+@markdown_available
 def test_markdown_section_render_html_toc() -> None:
     section = MarkdownSection(desc="### Hello Cat!")
     assert isinstance(Template(section.render_html_toc()).render(), str)
 
 
+@markdown_available
 def test_markdown_section_render_html_toc_args() -> None:
     section = MarkdownSection(desc="### Hello Cat!")
     assert isinstance(
