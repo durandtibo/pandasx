d71bfd08146aab4cae93eb8ce5e203c38a6923fd
Thibaut Durand <durand.tibo@gmail.com>
2024-07-11 00:07:05 -0700

    Improve `ColumnSubsetAnalyzer`

--- tests/unit/analyzer/test_column.py
+++ tests/unit/analyzer/test_column.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
-import numpy as np
-import pandas as pd
+import polars as pl
 from coola import objects_are_equal
 
 from flamme.analyzer import ColumnSubsetAnalyzer, NullValueAnalyzer
@@ -14,40 +13,48 @@ from flamme.section import NullValueSection
 
 def test_column_subset_analyzer_str() -> None:
     assert str(
-        ColumnSubsetAnalyzer(columns=["col1", "col2"], analyzer=NullValueAnalyzer())
+        ColumnSubsetAnalyzer(columns=["float", "str"], analyzer=NullValueAnalyzer())
     ).startswith("ColumnSubsetAnalyzer(")
 
 
 def test_column_subset_analyzer_get_statistics() -> None:
-    section = ColumnSubsetAnalyzer(columns=["col1", "col2"], analyzer=NullValueAnalyzer()).analyze(
-        pd.DataFrame(
+    section = ColumnSubsetAnalyzer(columns=["float", "str"], analyzer=NullValueAnalyzer()).analyze(
+        pl.DataFrame(
             {
-                "col1": np.array([1.2, 4.2, np.nan, 2.2]),
-                "col2": np.array([np.nan, 1, 0, 1]),
-                "col3": np.array(["A", "B", None, np.nan]),
-            }
+                "float": [1.2, 4.2, None, 2.2],
+                "int": [None, 1, 0, 1],
+                "str": ["A", "B", None, None],
+            },
+            schema={"float": pl.Float64, "int": pl.Int64, "str": pl.String},
         )
     )
     assert isinstance(section, NullValueSection)
     assert objects_are_equal(
         section.get_statistics(),
         {
-            "columns": ("col1", "col2"),
-            "null_count": (1, 1),
+            "columns": ("float", "str"),
+            "null_count": (1, 2),
             "total_count": (4, 4),
         },
     )
 
 
 def test_column_subset_analyzer_get_statistics_empty() -> None:
-    section = ColumnSubsetAnalyzer(columns=["col1", "col2"], analyzer=NullValueAnalyzer()).analyze(
-        pd.DataFrame({"col1": np.array([]), "col2": np.array([]), "col3": np.array([])})
+    section = ColumnSubsetAnalyzer(columns=["float", "str"], analyzer=NullValueAnalyzer()).analyze(
+        pl.DataFrame(
+            {
+                "float": [],
+                "int": [],
+                "str": [],
+            },
+            schema={"float": pl.Float64, "int": pl.Int64, "str": pl.String},
+        )
     )
     assert isinstance(section, NullValueSection)
     assert objects_are_equal(
         section.get_statistics(),
         {
-            "columns": ("col1", "col2"),
+            "columns": ("float", "str"),
             "null_count": (0, 0),
             "total_count": (0, 0),
         },
