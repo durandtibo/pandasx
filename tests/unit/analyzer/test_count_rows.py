7d0e752609c06fa6ee4672346d888995cc883696
Thibaut Durand <durand.tibo@gmail.com>
2024-07-11 22:09:13 -0700

    Improve `TemporalRowCountAnalyzer` (#539)

--- tests/unit/analyzer/test_count_rows.py
+++ tests/unit/analyzer/test_count_rows.py
@@ -1,29 +1,30 @@
 from __future__ import annotations
 
-import pandas as pd
+from datetime import datetime, timezone
+
+import polars as pl
 import pytest
 from coola import objects_are_equal
-from pandas.testing import assert_frame_equal
+from polars.testing import assert_frame_equal
 
 from flamme.analyzer import TemporalRowCountAnalyzer
 from flamme.section import EmptySection, TemporalRowCountSection
 
 
 @pytest.fixture()
-def dataframe() -> pd.DataFrame:
-    return pd.DataFrame(
+def dataframe() -> pl.DataFrame:
+    return pl.DataFrame(
         {
-            "datetime": pd.to_datetime(
-                [
-                    "2020-01-03",
-                    "2020-01-04",
-                    "2020-01-05",
-                    "2020-02-03",
-                    "2020-03-03",
-                    "2020-04-03",
-                ]
-            ),
-        }
+            "datetime": [
+                datetime(year=2020, month=1, day=3, tzinfo=timezone.utc),
+                datetime(year=2020, month=1, day=4, tzinfo=timezone.utc),
+                datetime(year=2020, month=1, day=5, tzinfo=timezone.utc),
+                datetime(year=2020, month=2, day=3, tzinfo=timezone.utc),
+                datetime(year=2020, month=3, day=3, tzinfo=timezone.utc),
+                datetime(year=2020, month=4, day=3, tzinfo=timezone.utc),
+            ]
+        },
+        schema={"datetime": pl.Datetime(time_unit="us", time_zone="UTC")},
     )
 
 
@@ -33,69 +34,62 @@ def dataframe() -> pd.DataFrame:
 
 
 def test_temporal_row_count_analyzer_str() -> None:
-    assert str(TemporalRowCountAnalyzer(dt_column="datetime", period="M")).startswith(
+    assert str(TemporalRowCountAnalyzer(dt_column="datetime", period="1mo")).startswith(
         "TemporalRowCountAnalyzer("
     )
 
 
-def test_temporal_row_count_analyzer_frame(dataframe: pd.DataFrame) -> None:
-    section = TemporalRowCountAnalyzer(dt_column="datetime", period="M").analyze(dataframe)
+def test_temporal_row_count_analyzer_frame(dataframe: pl.DataFrame) -> None:
+    section = TemporalRowCountAnalyzer(dt_column="datetime", period="1mo").analyze(dataframe)
     assert_frame_equal(section.frame, dataframe)
 
 
 @pytest.mark.parametrize("dt_column", ["datetime", "date"])
-def test_temporal_row_count_analyzer_dt_column(dt_column: str) -> None:
-    section = TemporalRowCountAnalyzer(dt_column=dt_column, period="M").analyze(
-        pd.DataFrame(
-            {
-                "datetime": pd.to_datetime(
-                    ["2020-01-03", "2020-02-03", "2020-03-03", "2020-04-03"]
-                ),
-                "date": pd.to_datetime(["2021-01-03", "2021-02-03", "2021-03-03", "2021-04-03"]),
-            }
-        )
+def test_temporal_row_count_analyzer_dt_column(dt_column: str, dataframe: pl.DataFrame) -> None:
+    section = TemporalRowCountAnalyzer(dt_column=dt_column, period="1mo").analyze(
+        dataframe.with_columns(pl.col("datetime").alias("date"))
     )
     assert section.dt_column == dt_column
 
 
-@pytest.mark.parametrize("period", ["M", "D"])
-def test_temporal_row_count_analyzer_period(dataframe: pd.DataFrame, period: str) -> None:
+@pytest.mark.parametrize("period", ["1mo", "1d"])
+def test_temporal_row_count_analyzer_period(dataframe: pl.DataFrame, period: str) -> None:
     section = TemporalRowCountAnalyzer(dt_column="datetime", period=period).analyze(dataframe)
     assert section.period == period
 
 
 @pytest.mark.parametrize("figsize", [(7, 3), (1.5, 1.5)])
 def test_temporal_row_count_analyzer_figsize(
-    dataframe: pd.DataFrame, figsize: tuple[int, int]
+    dataframe: pl.DataFrame, figsize: tuple[int, int]
 ) -> None:
-    section = TemporalRowCountAnalyzer(dt_column="datetime", period="M", figsize=figsize).analyze(
+    section = TemporalRowCountAnalyzer(dt_column="datetime", period="1mo", figsize=figsize).analyze(
         dataframe
     )
     assert section.figsize == figsize
 
 
-def test_temporal_row_count_analyzer_figsize_default(dataframe: pd.DataFrame) -> None:
-    section = TemporalRowCountAnalyzer(dt_column="datetime", period="M").analyze(dataframe)
+def test_temporal_row_count_analyzer_figsize_default(dataframe: pl.DataFrame) -> None:
+    section = TemporalRowCountAnalyzer(dt_column="datetime", period="1mo").analyze(dataframe)
     assert section.figsize is None
 
 
-def test_temporal_row_count_analyzer_get_statistics(dataframe: pd.DataFrame) -> None:
-    section = TemporalRowCountAnalyzer(dt_column="datetime", period="M").analyze(dataframe)
+def test_temporal_row_count_analyzer_get_statistics(dataframe: pl.DataFrame) -> None:
+    section = TemporalRowCountAnalyzer(dt_column="datetime", period="1mo").analyze(dataframe)
     assert isinstance(section, TemporalRowCountSection)
     assert objects_are_equal(section.get_statistics(), {})
 
 
 def test_temporal_row_count_analyzer_get_statistics_empty_rows() -> None:
-    section = TemporalRowCountAnalyzer(dt_column="datetime", period="M").analyze(
-        pd.DataFrame({"datetime": []})
+    section = TemporalRowCountAnalyzer(dt_column="datetime", period="1mo").analyze(
+        pl.DataFrame({"datetime": []})
     )
     assert isinstance(section, TemporalRowCountSection)
     assert objects_are_equal(section.get_statistics(), {})
 
 
 def test_temporal_row_count_analyzer_get_statistics_missing_dt_column() -> None:
-    section = TemporalRowCountAnalyzer(dt_column="datetime", period="M").analyze(
-        pd.DataFrame({"col": []})
+    section = TemporalRowCountAnalyzer(dt_column="datetime", period="1mo").analyze(
+        pl.DataFrame({"col": []})
     )
     assert isinstance(section, EmptySection)
     assert objects_are_equal(section.get_statistics(), {})
