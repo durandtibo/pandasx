8b3f7be9d120cca4458484cd44f63985c74ef40b
Thibaut Durand <durand.tibo@gmail.com>
2024-07-12 10:51:00 -0700

    Add `auto_yscale_discrete` (#551)

--- src/flamme/plot/utils/scale.py
+++ src/flamme/plot/utils/scale.py
@@ -2,7 +2,7 @@ r"""Contain scale utility functions."""
 
 from __future__ import annotations
 
-__all__ = ["auto_yscale_continuous"]
+__all__ = ["auto_yscale_continuous", "auto_yscale_discrete"]
 
 
 import numpy as np
@@ -41,3 +41,33 @@ def auto_yscale_continuous(array: np.ndarray, nbins: int | None = None) -> str:
     if np.nanmin(array) <= 0.0:
         return "symlog"
     return "log"
+
+
+def auto_yscale_discrete(min_count: int, max_count: int, threshold: int = 50) -> str:
+    r"""Find a good scale for y-axis based on the data distribution.
+
+    Args:
+        min_count: The minimal count value.
+        max_count: The maximal count value.
+        threshold: The threshold used to control the transition from
+            linear to log scale. ``50`` means the ratio
+            ``max_count/min_count`` must be greater than 50 to use the
+            log scale. If it is lower than 50, the linear scale is
+            used.
+
+    Returns:
+        The scale for y-axis.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from flamme.plot.utils import auto_yscale_discrete
+    >>> auto_yscale_discrete(min_count=5, max_count=10)
+    linear
+    >>> auto_yscale_discrete(min_count=5, max_count=1000)
+    log
+
+    ```
+    """
+    return "log" if (max_count / max(min_count, 1)) >= threshold else "linear"
