5f77bc18d7e90d2d1e9789d996368dfc8d79c5da
Thibaut Durand <durand.tibo@gmail.com>
2024-07-11 23:47:21 -0700

    Add `compute_unique_count` (#545)

--- tests/unit/utils/test_count.py
+++ tests/unit/utils/test_count.py
@@ -5,7 +5,50 @@ import polars as pl
 import pytest
 from coola import objects_are_equal
 
-from flamme.utils.count import row_temporal_count
+from flamme.utils.count import compute_unique_count, row_temporal_count
+
+#########################################
+#    Tests for compute_unique_count     #
+#########################################
+
+
+def test_compute_unique_count() -> None:
+    assert objects_are_equal(
+        compute_unique_count(
+            frame=pl.DataFrame(
+                {
+                    "int": [None, 1, 0, 1],
+                    "float": [1.2, 4.2, None, 2.2],
+                    "str": ["A", "B", None, None],
+                },
+                schema={"int": pl.Int64, "float": pl.Float64, "str": pl.String},
+            ),
+        ),
+        np.array([3, 4, 3], dtype=np.int64),
+    )
+
+
+def test_compute_unique_count_empty_rows() -> None:
+    assert objects_are_equal(
+        compute_unique_count(
+            frame=pl.DataFrame(
+                {"int": [], "float": [], "str": []},
+                schema={"int": pl.Int64, "float": pl.Float64, "str": pl.String},
+            ),
+        ),
+        np.array([0, 0, 0], dtype=np.int64),
+    )
+
+
+def test_compute_unique_count_empty() -> None:
+    assert objects_are_equal(
+        compute_unique_count(frame=pl.DataFrame({})), np.array([], dtype=np.int64)
+    )
+
+
+#######################################
+#    Tests for row_temporal_count     #
+#######################################
 
 
 @pytest.fixture()
@@ -23,11 +66,6 @@ def dataframe() -> pl.DataFrame:
         },
         schema={"datetime": pl.Datetime(time_unit="us", time_zone="UTC")},
     )
-
-
-#######################################
-#    Tests for row_temporal_count     #
-#######################################
 
 
 def test_row_temporal_count_month(dataframe: pl.DataFrame) -> None:
