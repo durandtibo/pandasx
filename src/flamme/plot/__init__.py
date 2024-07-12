c8dfc0cf67a50e8c44bc369a6d7165cee76b4fa8
Thibaut Durand <durand.tibo@gmail.com>
2024-07-12 11:23:11 -0700

    Add `bar_discrete` (#552)

--- src/flamme/plot/__init__.py
+++ src/flamme/plot/__init__.py
@@ -3,12 +3,14 @@ r"""Contain plotting functionalities."""
 from __future__ import annotations
 
 __all__ = [
-    "plot_null_temporal",
-    "plot_cdf",
     "hist_continuous",
     "hist_continuous2",
+    "bar_discrete",
+    "plot_cdf",
+    "plot_null_temporal",
 ]
 
 from flamme.plot.cdf import plot_cdf
 from flamme.plot.continuous import hist_continuous, hist_continuous2
+from flamme.plot.discrete import bar_discrete
 from flamme.plot.null_temp import plot_null_temporal
