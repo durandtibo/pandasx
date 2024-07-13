91c50613ecf7700e773c922e81ff6fa0fc26e196
Thibaut Durand <durand.tibo@gmail.com>
2024-07-13 10:04:02 -0700

    Improve `MostFrequentValuesSection`

--- tests/unit/section/test_most_frequent.py
+++ tests/unit/section/test_most_frequent.py
@@ -6,6 +6,11 @@ from coola import objects_are_allclose
 from jinja2 import Template
 
 from flamme.section import MostFrequentValuesSection
+from flamme.section.most_frequent import (
+    create_section_template,
+    create_table,
+    create_table_row,
+)
 
 ###############################################
 #     Tests for MostFrequentValuesSection     #
@@ -65,3 +70,38 @@ def test_most_frequent_values_section_render_html_toc_args() -> None:
     assert isinstance(
         Template(section.render_html_toc(number="1.", tags=["meow"], depth=1)).render(), str
     )
+
+
+#############################################
+#     Tests for create_section_template     #
+#############################################
+
+
+def test_create_section_template() -> None:
+    assert isinstance(create_section_template(), str)
+
+
+##################################
+#     Tests for create_table     #
+##################################
+
+
+def test_create_table() -> None:
+    assert isinstance(create_table(Counter({"a": 4, "b": 2, "c": 6})), str)
+
+
+def test_create_table_empty() -> None:
+    assert isinstance(create_table(Counter({})), str)
+
+
+######################################
+#     Tests for create_table_row     #
+######################################
+
+
+def test_create_table_row() -> None:
+    assert isinstance(create_table_row(Counter({"a": 4, "b": 2, "c": 6})), str)
+
+
+def test_create_table_row_empty() -> None:
+    assert isinstance(create_table_row(Counter({})), str)
