0ed4451ab77139235533656271261ca912b8c830
Thibaut Durand <durand.tibo@gmail.com>
2024-07-14 08:44:36 -0700

    Add `compute_temporal_value_counts` (#574)

--- src/flamme/utils/count.py
+++ src/flamme/utils/count.py
@@ -2,11 +2,13 @@ r"""Contain utility functions for counting."""
 
 from __future__ import annotations
 
-__all__ = ["compute_nunique", "compute_temporal_count"]
+__all__ = ["compute_nunique", "compute_temporal_count", "compute_temporal_value_counts"]
 
 import numpy as np
 import polars as pl
 from grizz.utils.period import period_to_strftime_format
+
+from flamme.utils.sorting import mixed_typed_sort
 
 
 def compute_nunique(frame: pl.DataFrame) -> np.ndarray:
@@ -58,7 +60,7 @@ def compute_temporal_count(
         period: The temporal period e.g. monthly or daily.
 
     Returns:
-        A tuple with the counts and the temporal window labels.
+        A tuple with the counts and the temporal steps.
 
     Example usage:
 
@@ -67,7 +69,7 @@ def compute_temporal_count(
     >>> from datetime import datetime, timezone
     >>> import polars as pl
     >>> from flamme.utils.count import compute_temporal_count
-    >>> counts, labels = compute_temporal_count(
+    >>> counts, steps = compute_temporal_count(
     ...     frame=pl.DataFrame(
     ...         {
     ...             "col1": [None, float("nan"), 0.0, 1.0, 4.2, 42.0],
@@ -92,7 +94,7 @@ def compute_temporal_count(
     ... )
     >>> counts
     array([3, 1, 1, 1])
-    >>> labels
+    >>> steps
     ['2020-01', '2020-02', '2020-03', '2020-04']
 
     ```
@@ -106,6 +108,93 @@ def compute_temporal_count(
         .group_by_dynamic("datetime", every=period)
     )
     format_dt = period_to_strftime_format(period)
-    labels = [name[0].strftime(format_dt) for name, _ in groups]
+    steps = [name[0].strftime(format_dt) for name, _ in groups]
     counts = groups.agg(pl.col("count").sum())["count"].to_numpy().astype(np.int64)
-    return counts, labels
+    return counts, steps
+
+
+def compute_temporal_value_counts(
+    frame: pl.DataFrame,
+    column: str,
+    dt_column: str,
+    period: str,
+) -> tuple[np.ndarray, list[str], list[str]]:
+    r"""Compute the value counts for temporal windows of a given column.
+
+    Args:
+        frame: The DataFrame to analyze.
+        column: The column to analyze the temporal value counts.
+        dt_column: The datetime column used to analyze
+            the temporal distribution.
+        period: The temporal period e.g. monthly or daily.
+
+    Returns:
+        A tuple with 3 items. The first item is a 2-d array that
+            indicates the number of occurrences for each value and
+            time step. The first dimension represents the value and
+            the second dimension represents the steps. The second item
+            is the list of time steps. The third item is the list of
+            string representation of the values.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from datetime import datetime, timezone
+    >>> import polars as pl
+    >>> from flamme.utils.count import compute_temporal_value_counts
+    >>> counts, steps, values = compute_temporal_value_counts(
+    ...     frame=pl.DataFrame(
+    ...         {
+    ...             "col1": [None, 1.0, 0.0, 1.0, 4.2, 42.0],
+    ...             "col2": [None, 1, 0, None, 2, 3],
+    ...             "datetime": [
+    ...                 datetime(year=2020, month=1, day=3, tzinfo=timezone.utc),
+    ...                 datetime(year=2020, month=1, day=4, tzinfo=timezone.utc),
+    ...                 datetime(year=2020, month=1, day=5, tzinfo=timezone.utc),
+    ...                 datetime(year=2020, month=2, day=3, tzinfo=timezone.utc),
+    ...                 datetime(year=2020, month=3, day=3, tzinfo=timezone.utc),
+    ...                 datetime(year=2020, month=4, day=3, tzinfo=timezone.utc),
+    ...             ],
+    ...         },
+    ...         schema={
+    ...             "col1": pl.Float64,
+    ...             "col2": pl.Int64,
+    ...             "datetime": pl.Datetime(time_unit="us", time_zone="UTC"),
+    ...         },
+    ...     ),
+    ...     column="col1",
+    ...     dt_column="datetime",
+    ...     period="1mo",
+    ... )
+    >>> counts
+    array([[1, 1, 0, 0, 1],
+           [0, 1, 0, 0, 0],
+           [0, 0, 1, 0, 0],
+           [0, 0, 0, 1, 0]])
+    >>> steps
+    ['2020-01', '2020-02', '2020-03', '2020-04']
+    >>> values
+    ['0.0', '1.0', '4.2', '42.0', 'null']
+
+    ```
+    """
+    if frame.shape[0] == 0:
+        return np.zeros((0, 0), dtype=np.int64), [], []
+
+    groups = (
+        frame.select(pl.col(dt_column).alias("__datetime__"), pl.col(column).alias("value"))
+        .sort(["__datetime__", "value"])
+        .group_by_dynamic("__datetime__", every=period)
+    )
+    format_dt = period_to_strftime_format(period)
+    steps = [name[0].strftime(format_dt) for name, _ in groups]
+    counts = (
+        groups.agg(pl.col("value").value_counts())  # noqa: PD010
+        .explode("value")
+        .unnest("value")
+        .pivot(index="__datetime__", columns="value", values="count")
+        .drop("__datetime__")
+    )
+    counts = counts.select(mixed_typed_sort(counts.columns))
+    return counts.fill_null(0.0).to_numpy().astype(np.int64), steps, list(counts.columns)
