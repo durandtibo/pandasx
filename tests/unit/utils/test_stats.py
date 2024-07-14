4e816061b81ea336d7f36d92f67f1a74154bc06b
Thibaut Durand <durand.tibo@gmail.com>
2024-07-14 14:25:37 -0700

    Rename and improve `compute_statistics_continuous_array` (#593)

--- tests/unit/utils/test_stats.py
+++ tests/unit/utils/test_stats.py
@@ -6,20 +6,25 @@ import pytest
 from coola import objects_are_allclose
 
 from flamme.utils.stats import (
-    compute_statistics_continuous,
+    compute_statistics_continuous_array,
     compute_statistics_continuous_series,
     quantile,
 )
 
-###################################################
-#     Tests for compute_statistics_continuous     #
-###################################################
+#########################################################
+#     Tests for compute_statistics_continuous_array     #
+#########################################################
 
 
-def test_compute_statistics_continuous() -> None:
+@pytest.mark.parametrize("dtype", [np.int32, np.int64, np.float32, np.float64])
+def test_compute_statistics_continuous_array(dtype: np.dtype) -> None:
     assert objects_are_allclose(
-        compute_statistics_continuous(np.arange(101)),
+        compute_statistics_continuous_array(np.arange(101, dtype=dtype)),
         {
+            "count": 101,
+            "num_nulls": 0,
+            "num_non_nulls": 101,
+            "nunique": 101,
             "mean": 50.0,
             "std": 29.154759474226502,
             "skewness": 0.0,
@@ -45,10 +50,14 @@ def test_compute_statistics_continuous() -> None:
     )
 
 
-def test_compute_statistics_continuous_empty() -> None:
+def test_compute_statistics_continuous_array_empty() -> None:
     assert objects_are_allclose(
-        compute_statistics_continuous(np.array([])),
+        compute_statistics_continuous_array(np.array([])),
         {
+            "count": 0,
+            "num_nulls": 0,
+            "num_non_nulls": 0,
+            "nunique": 0,
             "mean": float("nan"),
             "std": float("nan"),
             "skewness": float("nan"),
@@ -74,39 +83,48 @@ def test_compute_statistics_continuous_empty() -> None:
     )
 
 
-def test_compute_statistics_continuous_with_nan() -> None:
+def test_compute_statistics_continuous_array_with_nan() -> None:
     assert objects_are_allclose(
-        compute_statistics_continuous(np.array([np.nan, *list(range(101)), np.nan])),
+        compute_statistics_continuous_array(np.array([np.nan, *list(range(101)), np.nan])),
         {
-            "mean": float("nan"),
-            "std": float("nan"),
-            "skewness": float("nan"),
-            "kurtosis": float("nan"),
-            "min": float("nan"),
-            "q001": float("nan"),
-            "q01": float("nan"),
-            "q05": float("nan"),
-            "q10": float("nan"),
-            "q25": float("nan"),
-            "median": float("nan"),
-            "q75": float("nan"),
-            "q90": float("nan"),
-            "q95": float("nan"),
-            "q99": float("nan"),
-            "q999": float("nan"),
-            "max": float("nan"),
+            "count": 103,
+            "num_nulls": 2,
+            "num_non_nulls": 101,
+            "nunique": 102,
+            "mean": 50.0,
+            "std": 29.154759474226502,
+            "skewness": 0.0,
+            "kurtosis": -1.2,
+            "min": 0.0,
+            "q001": 0.1,
+            "q01": 1.0,
+            "q05": 5.0,
+            "q10": 10.0,
+            "q25": 25.0,
+            "median": 50.0,
+            "q75": 75.0,
+            "q90": 90.0,
+            "q95": 95.0,
+            "q99": 99.0,
+            "q999": 99.9,
+            "max": 100.0,
             ">0": 100,
             "<0": 0,
             "=0": 1,
         },
+        atol=1e-2,
         equal_nan=True,
     )
 
 
-def test_compute_statistics_continuous_single_numeric_value() -> None:
+def test_compute_statistics_continuous_array_single_numeric_value() -> None:
     assert objects_are_allclose(
-        compute_statistics_continuous(np.array([1, 1, 1, 1, 1])),
+        compute_statistics_continuous_array(np.array([1, 1, 1, 1, 1])),
         {
+            "count": 5,
+            "num_nulls": 0,
+            "num_non_nulls": 5,
+            "nunique": 1,
             "mean": 1.0,
             "std": 0.0,
             "skewness": float("nan"),
@@ -132,10 +150,14 @@ def test_compute_statistics_continuous_single_numeric_value() -> None:
     )
 
 
-def test_compute_statistics_continuous_only_nans() -> None:
+def test_compute_statistics_continuous_array_only_nans() -> None:
     assert objects_are_allclose(
-        compute_statistics_continuous(np.asarray([np.nan, np.nan, np.nan, np.nan])),
+        compute_statistics_continuous_array(np.asarray([np.nan, np.nan, np.nan, np.nan])),
         {
+            "count": 4,
+            "num_nulls": 4,
+            "num_non_nulls": 0,
+            "nunique": 1,
             "mean": float("nan"),
             "std": float("nan"),
             "skewness": float("nan"),
