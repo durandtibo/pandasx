2a2ae6541c7f343d296e44d6cba9c0e0548c5b71
Thibaut Durand <durand.tibo@gmail.com>
2024-07-15 07:02:53 -0700

    Improve `ColumnTemporalContinuousAnalyzer` (#618)

--- tests/unit/analyzer/test_continuous_temp.py
+++ tests/unit/analyzer/test_continuous_temp.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-import numpy as np
-import pandas as pd
+from datetime import datetime, timezone
+
+import polars as pl
 import pytest
 from coola import objects_are_equal
 
@@ -14,67 +15,78 @@ from flamme.section import ColumnTemporalContinuousSection, EmptySection
 
 
 @pytest.fixture()
-def dataframe() -> pd.DataFrame:
-    return pd.DataFrame(
+def dataframe() -> pl.DataFrame:
+    return pl.DataFrame(
         {
-            "col": np.array([1.2, 4.2, np.nan, 2.2]),
-            "datetime": pd.to_datetime(["2020-01-03", "2020-02-03", "2020-03-03", "2020-04-03"]),
-        }
+            "col": [0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
+            "datetime": [
+                datetime(year=2020, month=4, day=3, tzinfo=timezone.utc),
+                datetime(year=2020, month=1, day=1, tzinfo=timezone.utc),
+                datetime(year=2020, month=1, day=2, tzinfo=timezone.utc),
+                datetime(year=2020, month=1, day=3, tzinfo=timezone.utc),
+                datetime(year=2020, month=2, day=3, tzinfo=timezone.utc),
+                datetime(year=2020, month=3, day=3, tzinfo=timezone.utc),
+            ],
+        },
+        schema={
+            "col": pl.Float64,
+            "datetime": pl.Datetime(time_unit="us", time_zone="UTC"),
+        },
     )
 
 
 def test_column_temporal_continuous_analyzer_str() -> None:
     assert str(
-        ColumnTemporalContinuousAnalyzer(column="col", dt_column="datetime", period="M")
+        ColumnTemporalContinuousAnalyzer(column="col", dt_column="datetime", period="1mo")
     ).startswith("ColumnTemporalContinuousAnalyzer(")
 
 
-def test_column_temporal_continuous_analyzer_column(dataframe: pd.DataFrame) -> None:
+def test_column_temporal_continuous_analyzer_column(dataframe: pl.DataFrame) -> None:
     section = ColumnTemporalContinuousAnalyzer(
-        column="col", dt_column="datetime", period="M"
+        column="col", dt_column="datetime", period="1mo"
     ).analyze(dataframe)
     assert isinstance(section, ColumnTemporalContinuousSection)
     assert section.column == "col"
 
 
-def test_column_temporal_continuous_analyzer_dt_column(dataframe: pd.DataFrame) -> None:
+def test_column_temporal_continuous_analyzer_dt_column(dataframe: pl.DataFrame) -> None:
     section = ColumnTemporalContinuousAnalyzer(
-        column="col", dt_column="datetime", period="M"
+        column="col", dt_column="datetime", period="1mo"
     ).analyze(dataframe)
     assert isinstance(section, ColumnTemporalContinuousSection)
     assert section.dt_column == "datetime"
 
 
-def test_column_temporal_continuous_analyzer_period(dataframe: pd.DataFrame) -> None:
+def test_column_temporal_continuous_analyzer_period(dataframe: pl.DataFrame) -> None:
     section = ColumnTemporalContinuousAnalyzer(
-        column="col", dt_column="datetime", period="M"
+        column="col", dt_column="datetime", period="1mo"
     ).analyze(dataframe)
     assert isinstance(section, ColumnTemporalContinuousSection)
-    assert section.period == "M"
+    assert section.period == "1mo"
 
 
-def test_column_temporal_continuous_analyzer_yscale_default(dataframe: pd.DataFrame) -> None:
+def test_column_temporal_continuous_analyzer_yscale_default(dataframe: pl.DataFrame) -> None:
     section = ColumnTemporalContinuousAnalyzer(
-        column="col", dt_column="datetime", period="M"
+        column="col", dt_column="datetime", period="1mo"
     ).analyze(dataframe)
     assert isinstance(section, ColumnTemporalContinuousSection)
     assert section.yscale == "auto"
 
 
 @pytest.mark.parametrize("yscale", ["linear", "log"])
-def test_column_temporal_continuous_analyzer_yscale(dataframe: pd.DataFrame, yscale: str) -> None:
+def test_column_temporal_continuous_analyzer_yscale(dataframe: pl.DataFrame, yscale: str) -> None:
     section = ColumnTemporalContinuousAnalyzer(
-        column="col", dt_column="datetime", period="M", yscale=yscale
+        column="col", dt_column="datetime", period="1mo", yscale=yscale
     ).analyze(dataframe)
     assert isinstance(section, ColumnTemporalContinuousSection)
     assert section.yscale == yscale
 
 
-def test_column_temporal_continuous_analyzer_figsize_default(dataframe: pd.DataFrame) -> None:
+def test_column_temporal_continuous_analyzer_figsize_default(dataframe: pl.DataFrame) -> None:
     section = ColumnTemporalContinuousAnalyzer(
         column="col",
         dt_column="datetime",
-        period="M",
+        period="1mo",
     ).analyze(dataframe)
     assert isinstance(section, ColumnTemporalContinuousSection)
     assert section.figsize is None
@@ -82,18 +94,18 @@ def test_column_temporal_continuous_analyzer_figsize_default(dataframe: pd.DataF
 
 @pytest.mark.parametrize("figsize", [(7, 3), (1.5, 1.5)])
 def test_column_temporal_continuous_analyzer_figsize(
-    dataframe: pd.DataFrame, figsize: tuple[float, float]
+    dataframe: pl.DataFrame, figsize: tuple[float, float]
 ) -> None:
     section = ColumnTemporalContinuousAnalyzer(
-        column="col", dt_column="datetime", period="M", figsize=figsize
+        column="col", dt_column="datetime", period="1mo", figsize=figsize
     ).analyze(dataframe)
     assert isinstance(section, ColumnTemporalContinuousSection)
     assert section.figsize == figsize
 
 
-def test_column_temporal_continuous_analyzer_analyze(dataframe: pd.DataFrame) -> None:
+def test_column_temporal_continuous_analyzer_analyze(dataframe: pl.DataFrame) -> None:
     section = ColumnTemporalContinuousAnalyzer(
-        column="col", dt_column="datetime", period="M"
+        column="col", dt_column="datetime", period="1mo"
     ).analyze(dataframe)
     assert isinstance(section, ColumnTemporalContinuousSection)
     assert objects_are_equal(section.get_statistics(), {})
@@ -101,32 +113,42 @@ def test_column_temporal_continuous_analyzer_analyze(dataframe: pd.DataFrame) ->
 
 def test_column_temporal_continuous_analyzer_analyze_empty() -> None:
     section = ColumnTemporalContinuousAnalyzer(
-        column="col", dt_column="datetime", period="M"
-    ).analyze(pd.DataFrame({"col": [], "int": [], "str": [], "datetime": []}))
+        column="col", dt_column="datetime", period="1mo"
+    ).analyze(pl.DataFrame({"col": [], "datetime": []}))
     assert isinstance(section, ColumnTemporalContinuousSection)
     assert objects_are_equal(section.get_statistics(), {})
 
 
 def test_column_temporal_continuous_analyzer_analyze_missing_column() -> None:
     section = ColumnTemporalContinuousAnalyzer(
-        column="col", dt_column="datetime", period="M"
-    ).analyze(pd.DataFrame({"datetime": []}))
+        column="col", dt_column="datetime", period="1mo"
+    ).analyze(pl.DataFrame({"datetime": []}))
     assert isinstance(section, EmptySection)
     assert objects_are_equal(section.get_statistics(), {})
 
 
 def test_column_temporal_continuous_analyzer_analyze_missing_dt_column() -> None:
     section = ColumnTemporalContinuousAnalyzer(
-        column="col", dt_column="datetime", period="M"
-    ).analyze(pd.DataFrame({"col": []}))
+        column="col", dt_column="datetime", period="1mo"
+    ).analyze(pl.DataFrame({"col": []}))
     assert isinstance(section, EmptySection)
     assert objects_are_equal(section.get_statistics(), {})
 
 
 def test_column_temporal_continuous_analyzer_analyze_same_column() -> None:
-    section = ColumnTemporalContinuousAnalyzer(column="col", dt_column="col", period="M").analyze(
-        pd.DataFrame(
-            {"col": pd.to_datetime(["2020-01-03", "2020-02-03", "2020-03-03", "2020-04-03"])}
+    section = ColumnTemporalContinuousAnalyzer(column="col", dt_column="col", period="1mo").analyze(
+        pl.DataFrame(
+            {
+                "col": [
+                    datetime(year=2020, month=4, day=3, tzinfo=timezone.utc),
+                    datetime(year=2020, month=1, day=1, tzinfo=timezone.utc),
+                    datetime(year=2020, month=1, day=2, tzinfo=timezone.utc),
+                    datetime(year=2020, month=1, day=3, tzinfo=timezone.utc),
+                    datetime(year=2020, month=2, day=3, tzinfo=timezone.utc),
+                    datetime(year=2020, month=3, day=3, tzinfo=timezone.utc),
+                ],
+            },
+            schema={"col": pl.Datetime(time_unit="us", time_zone="UTC")},
         )
     )
     assert isinstance(section, EmptySection)
