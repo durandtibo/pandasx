81fdc6471984e3a5537df54cee3928509dc2015c
Thibaut Durand <durand.tibo@gmail.com>
2024-07-15 09:14:23 -0700

    Clean reporters (#624)

--- src/flamme/reporter/vanilla.py
+++ src/flamme/reporter/vanilla.py
@@ -28,8 +28,7 @@ class Reporter(BaseReporter):
 
     Args:
         ingestor: The ingestor or its configuration.
-        transformer: Specifies a ``pandas.DataFrame`` transformer or
-            its configuration.
+        transformer: The data transformer or its configuration.
         ingestor: The analyzer or its configuration.
         report_path: The path where to save the HTML report.
         max_toc_depth: The maximum level to show in the
