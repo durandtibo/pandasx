5560a6a138043df02af7d63d2e1a937f6652e847
Thibaut Durand <durand.tibo@gmail.com>
2024-07-14 21:23:31 -0700

    Add `to_temporal_frames`

--- tests/unit/utils/test_temporal.py
+++ tests/unit/utils/test_temporal.py
@@ -103,7 +103,30 @@ def test_to_temporal_frames_monthly(dataframe: pl.DataFrame) -> None:
 def test_to_temporal_frames_yearly(dataframe: pl.DataFrame) -> None:
     assert objects_are_equal(
         to_temporal_frames(dataframe, dt_column="datetime", period="1y"),
-        ([dataframe], ["2020"]),
+        (
+            [
+                pl.DataFrame(
+                    {
+                        "col1": [1.0, 2.0, 3.0, 4.0, 5.0, 0.0],
+                        "col2": [2, 3, 4, 5, 6, 1],
+                        "datetime": [
+                            datetime(year=2020, month=1, day=1, tzinfo=timezone.utc),
+                            datetime(year=2020, month=1, day=2, tzinfo=timezone.utc),
+                            datetime(year=2020, month=1, day=3, tzinfo=timezone.utc),
+                            datetime(year=2020, month=2, day=3, tzinfo=timezone.utc),
+                            datetime(year=2020, month=3, day=3, tzinfo=timezone.utc),
+                            datetime(year=2020, month=4, day=3, tzinfo=timezone.utc),
+                        ],
+                    },
+                    schema={
+                        "col1": pl.Float64,
+                        "col2": pl.Int64,
+                        "datetime": pl.Datetime(time_unit="us", time_zone="UTC"),
+                    },
+                )
+            ],
+            ["2020"],
+        ),
     )
 
 
