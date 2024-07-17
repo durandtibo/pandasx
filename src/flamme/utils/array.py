9acff48fa1083ce728c9b589199e32ca0994e282
Thibaut Durand <durand.tibo@gmail.com>
2024-07-16 22:50:27 -0700

    Add `filter_range`

--- src/flamme/utils/array.py
+++ src/flamme/utils/array.py
@@ -2,11 +2,37 @@ r"""Contain utility functions for NumPy arrays."""
 
 from __future__ import annotations
 
-__all__ = ["nonnan", "rand_replace"]
+__all__ = ["filter_range", "nonnan", "rand_replace"]
 
 from typing import Any
 
 import numpy as np
+
+
+def filter_range(array: np.ndarray, xmin: float, xmax: float) -> np.ndarray:
+    r"""Filter in the values in a given range.
+
+    Args:
+        array: The input array.
+        xmin: The lower bound of the range.
+        xmax: The upper bound of the range.
+
+    Returns:
+        A 1-d array with only the values in the given range.
+
+    Example usage:
+
+    ```pycon
+
+    >>> import numpy as np
+    >>> from flamme.utils.array import filter_range
+    >>> out = filter_range(np.arange(10), xmin=-1, xmax=5)
+    >>> out
+    array([0, 1, 2, 3, 4, 5])
+
+    ```
+    """
+    return np.extract(np.logical_and(xmin <= array, array <= xmax), array)
 
 
 def nonnan(array: np.ndarray) -> np.ndarray:
--- tests/unit/utils/test_array.py
+++ tests/unit/utils/test_array.py
@@ -1,9 +1,34 @@
 from __future__ import annotations
 
 import numpy as np
+import pytest
 from coola import objects_are_equal
 
-from flamme.utils.array import nonnan, rand_replace
+from flamme.utils.array import filter_range, nonnan, rand_replace
+
+##################################
+#     Tests for filter_range     #
+##################################
+
+
+@pytest.mark.parametrize("dtype", [np.int64, np.float64])
+def test_filter_range(dtype: np.dtype) -> None:
+    assert np.array_equal(
+        filter_range(np.arange(10, dtype=dtype), xmin=1, xmax=5),
+        np.array([1, 2, 3, 4, 5], dtype=dtype),
+    )
+
+
+def test_filter_range_inf() -> None:
+    assert np.array_equal(
+        filter_range(np.arange(10), xmin=float("-inf"), xmax=float("inf")),
+        np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+    )
+
+
+def test_filter_range_empty() -> None:
+    assert np.array_equal(filter_range(np.array([]), xmin=1, xmax=5), np.array([]))
+
 
 ############################
 #     Tests for nonnan     #
@@ -11,18 +36,18 @@ from flamme.utils.array import nonnan, rand_replace
 
 
 def test_nonnan_empty() -> None:
-    assert np.array_equal(nonnan(np.asarray([])), np.asarray([]))
+    assert np.array_equal(nonnan(np.array([])), np.array([]))
 
 
 def test_nonnan_1d() -> None:
     assert np.array_equal(
-        nonnan(np.asarray([1, 2, float("nan"), 5, 6])), np.asarray([1.0, 2.0, 5.0, 6.0])
+        nonnan(np.array([1, 2, float("nan"), 5, 6])), np.array([1.0, 2.0, 5.0, 6.0])
     )
 
 
 def test_nonnan_2d() -> None:
     assert np.array_equal(
-        nonnan(np.asarray([[1, 2, float("nan")], [4, 5, 6]])), np.asarray([1.0, 2.0, 4.0, 5.0, 6.0])
+        nonnan(np.array([[1, 2, float("nan")], [4, 5, 6]])), np.array([1.0, 2.0, 4.0, 5.0, 6.0])
     )
 
 
